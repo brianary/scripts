fd25eb20c9aee3cfd2516d765bb716fd112425e5
Brian Lalonde <brianl@stcu.org>
2015-04-06 09:26:49 -0700

    Script to files, improve filters & options
    Script to files to preserve terminators, filter empty collections,
    fixed roles, add comments, accept scripting option params.

--- Export-DatabaseScripts.ps1
+++ Export-DatabaseScripts.ps1
@@ -5,8 +5,10 @@ Exports MS SQL database structures from the given server and database as files, 
 The name of the server (and instance) to connect to.
 .Parameter Database
 The name of the database to connect to on the server.
-.Parameter CreateEmptyFolders
-Creates folders even if no scripts will be exported to them.
+.Parameter Encoding
+The file encoding to use for the SQL scripts.
+.Parameter ScriptingOptions
+Provides a list of boolean SMO ScriptingOptions properties to set to true.
 .Component
 Microsoft.SqlServer.ConnectionInfo
 .Component
@@ -14,16 +16,25 @@ Microsoft.SqlServer.Smo
 .Component
 Microsoft.SqlServer.SqlEnum
 .Example
-Export-SqlScripts.ps1 "(localdb)\ProjectV12" AdventureWorks2014
+Export-SqlScripts.ps1 ServerName\instance AdventureWorks2014
 (outputs SQL scripts to files)
+.Link
+https://msdn.microsoft.com/en-us/library/microsoft.sqlserver.management.smo.aspx
+.Link
+https://msdn.microsoft.com/en-us/library/microsoft.sqlserver.management.smo.scriptingoptions_properties.aspx
 #>
 
 #requires -version 3
 [CmdletBinding()] Param(
 [Parameter(Position=0,Mandatory=$true)][string] $Server,
-[Parameter(Position=1,Mandatory=$true)][string] $Database
+[Parameter(Position=1,Mandatory=$true)][string] $Database,
+[Parameter(Position=2)][ValidateSet('Unicode','UTF7','UTF8','UTF32','ASCII','BigEndianUnicode','Default','OEM')][string]$Encoding = 'UTF8',
+[Parameter(Position=3,ValueFromRemainingArguments=$true)][string[]] $ScriptingOptions = (@'
+EnforceScriptingOptions ExtendedProperties Permissions DriAll Indexes Triggers ScriptBatchTerminator
+'@.Trim() -split '\W+')
 )
 
+# load SMO
 try
 {
     [Microsoft.SqlServer.Management.Smo.Server]|Out-Null
@@ -42,15 +53,28 @@ catch
     Add-Type -Path "$sqlsdk\Microsoft.SqlServer.SqlEnum.dll"
 }
 
+# connect to database
 $srv = New-Object Microsoft.SqlServer.Management.Smo.Server($Server)
 $db = $srv.Databases[$Database]
 if(!$db) {return}
 Write-Verbose "Connected to $srv.$db $($srv.Product) $($srv.Edition) $($srv.VersionString) $($srv.ProductLevel) (Windows $($srv.OSVersion))"
 mkdir $Database -EA:SilentlyContinue |Out-Null ; pushd $Database
 
+# set up scripting options
 $opts = New-Object Microsoft.SqlServer.Management.Smo.ScriptingOptions
-'ExtendedProperties,Permissions,DriAll,Indexes,Triggers'.Trim() -split '\W+' |% {$opts.$_ = $true}
+$ScriptingOptions |
+    % {
+        if(($opts |Get-Member $_) -and $opts.$_ -is [bool]) {$opts.$_ = $true} 
+        else {Write-Warning "Not a boolean scripting option: '$_'"}
+    }
+$opts.ToFileOnly = $true
+$opts.Encoding = 
+    if($Encoding -eq 'OEM') {$OutputEncoding.GetEncoder().Encoding}
+    else {[Text.Encoding]::$Encoding}
+Write-Verbose "Scripting options flags: $(($opts |Get-Member -MemberType Property |% Name |? {$opts.$_ -is [bool] -and $opts.$_}) -join ', ')"
+Write-Verbose "Scripting options values: $(($opts |Get-Member -MemberType Property |% Name |? {$opts.$_ -isnot [bool] -and $opts.$_} |% {"$_=$($opts.$_)"}) -join ', ')"
 
+# map database SMO object collection properties to folder structure
 $folder = @{
     # Property           = # Folder
     Assemblies           = 'Assemblies'
@@ -89,21 +113,26 @@ $brkr = @{ # not something we currently use
 
 function ConvertTo-FileName($Name) { $Name -replace '[<>\\/"|\t]+','_' }
 
+# script all mapped SMO object collection properties
+# skip collections that are empty, only system objects, and only fixed roles
+# skip system objects and fixed roles
 $folder.Keys |
-    ? {($db.$_ -isnot [Microsoft.SqlServer.Management.Smo.DatabaseRoleCollection]) -or ($db.$_ |? IsFixedRole -eq $false)} |
-    ? {!($db.$_ |gm IsSystemObject) -or ($db.$_ |? IsSystemObject -eq $false)} |
+    ? {$db.$_.Count} |
+    ? {$db.$_ -isnot [Microsoft.SqlServer.Management.Smo.DatabaseRoleCollection] -or ($db.$_ |? IsFixedRole -eq $false)} |
+    ? {!($db.$_ |Get-Member IsSystemObject) -or ($db.$_ |? IsSystemObject -eq $false)} |
     % {
         mkdir $folder.$_ -EA:SilentlyContinue |Out-Null ; pushd $folder.$_
         $db.$_ |
-            ? {!($_|gm IsSystemObject) -or !($_.IsSystemObject)} |
+            ? {!($_|Get-Member IsSystemObject) -or !($_.IsSystemObject)} |
+            ? {$_ -isnot [Microsoft.SqlServer.Management.Smo.DatabaseRole] -or !($_.IsFixedRole)} |
             % {
-                $filename = 
-                    if($_|gm Schema) {"$(ConvertTo-FileName $_.Schema).$(ConvertTo-FileName $_.Name).sql"}
-                    else {"$(ConvertTo-FileName $_.Name).sql"}
-                Write-Verbose "Export $($_.GetType().Name) $_ to $filename"
-                $_.Script($opts) |Out-File $filename
+                $opts.FileName = 
+                    if($_|Get-Member Schema) {"$pwd\$(ConvertTo-FileName $_.Schema).$(ConvertTo-FileName $_.Name).sql"}
+                    else {"$pwd\$(ConvertTo-FileName $_.Name).sql"}
+                Write-Verbose "Export $($_.GetType().Name) $_ to $($opts.FileName)"
+                $_.Script($opts)
             }
-        popd
+        popd # object collection folder
     }
 
 popd # Database
