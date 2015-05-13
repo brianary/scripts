b0a3eb18c3b39ea184d503cffb7b02c572df85cb
Brian Lalonde <brianary@gmail.com>
2015-05-13 16:58:03 -0700

    Convert data elements to PSObjects.

---
+++ ConvertFrom-EscapedXml.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.Synopsis
+Parse escaped XML into XML and serialize it.
+.Parameter EscapedXml
+The escaped XML text.
+.Parameter NoIndent
+Outputs the XML without indentation.
+#>
+
+#request -version 2
+[CmdletBinding()] Param(
+[Parameter(Mandatory=$true,Position=0)][string]$EscapedXml,
+[switch]$NoIndent
+)
+Add-Type -as System.Web
+[xml] $xml = [Web.HttpUtility]::HtmlDecode($EscapedXml)
+$sw = New-Object IO.StringWriter
+[Xml.XmlWriterSettings] $saveopts = New-Object Xml.XmlWriterSettings -Property @{ Indent = $true }
+[Xml.XmlWriter] $xo = [Xml.XmlWriter]::Create($sw, $saveopts)
+$xml.WriteTo($xo)
+$xo.Dispose()
+$out = $sw.ToString()
+$sw.Dispose()
+$out
