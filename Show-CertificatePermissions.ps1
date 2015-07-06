30c8215a68025efa98d62a25b01376a465cf791f
Brian Lalonde <brianl@stcu.org>
2015-07-06 12:59:48 -0700

    Provide visibility into certificate permissions.

---
+++ Show-CertificatePermissions.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.Synopsis
+    Shows the permissions of a certificate's private key file.
+
+.Parameter Certificate
+    The certificate to display permissions for.
+
+.Link
+    Find-Certificate.ps1
+
+.Example
+    Find-Certificate.ps1 -FindValue ExampleCert -FindType FindBySubjectName -StoreName TrustedPeople -StoreLocation LocalMachine |Show-CertificatePermissions
+    Displays the permissions for the certificate.
+#>
+
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)]
+[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate
+)
+ls "$env:ProgramData\Microsoft\Crypto\RSA\MachineKeys","$env:APPDATA\Microsoft\Crypto\RSA" -Recurse -Filter $cert.PrivateKey.CspKeyContainerInfo.UniqueKeyContainerName |% FullName |% {icacls $_}
