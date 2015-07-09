846d1adc697bdfe13735981c6102327865d4f738
Brian Lalonde <brianl@stcu.org>
2015-07-09 14:48:59 -0700

    Trivial XML formatting.

---
+++ Format-Xml.ps1
@@ -0,0 +1,10 @@
+﻿<#
+.Synopsis
+    Pretty-print XML.
+
+.Parameter Xml
+    The XML string or document to format.
+#>
+
+[CmdletBinding()] Param([Parameter(Position=0,Mandatory=$true)][xml]$Xml)
+$Xml.Save([Console]::Out)
