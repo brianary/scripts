3f65b262ee89a2265edcef55b31aa13d8a4ba531
Brian Lalonde <brianary@gmail.com>
2015-08-19 08:51:06 -0700

    System details script. Better .NET Framework return type.

---
+++ ConvertTo-ByteUnits.ps1
@@ -0,0 +1,43 @@
+﻿<#
+.Synopsis
+Converts bytes to largest possible units, to improve readability.
+.Parameter Bytes
+The number of bytes to express in larger units.
+.Parameter Precision
+The maximum number of digits after the decimal to keep.
+The default is 16 (the maximum).
+.Parameter UseSI
+Displays unambiguous SI units (with a space).
+By default, native PowerShell units are used
+(without a space, to allow round-tripping the value,
+though there may be significant rounding loss depending on precision).
+.Example
+ConvertTo-ByteUnits 65536
+64KB
+.Example
+ConvertTo-ByteUnits 9685059 -dot 1 -si
+9.2 MiB
+.Example
+ls *.log |measure -sum Length |select -exp Sum |ConvertTo-ByteUnits -dot 2 -si
+302.39 MiB
+.Inputs
+A integer (up to 64-bit) representing a number of bytes.
+.Link
+http://en.wikipedia.org/wiki/Binary_prefix
+.Link
+http://physics.nist.gov/cuu/Units/binary.html
+#>
+
+#requires -version 2
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][int64]$Bytes,
+[Alias('Digits','dot')][ValidateRange(0,16)][byte]$Precision = 16,
+[Alias('si')][switch]$UseSI
+)
+$pfmt = New-Object String '#',$Precision
+if($bytes -gt 1PB)     {"{0:0.$pfmt}{1}" -f ($bytes / 1PB),@("PB"," PiB")[$UseSI.IsPresent]}
+elseif($bytes -gt 1TB) {"{0:0.$pfmt}{1}" -f ($bytes / 1TB),@("TB"," TiB")[$UseSI.IsPresent]}
+elseif($bytes -gt 1GB) {"{0:0.$pfmt}{1}" -f ($bytes / 1GB),@("GB"," GiB")[$UseSI.IsPresent]}
+elseif($bytes -gt 1MB) {"{0:0.$pfmt}{1}" -f ($bytes / 1MB),@("MB"," MiB")[$UseSI.IsPresent]}
+elseif($bytes -gt 1KB) {"{0:0.$pfmt}{1}" -f ($bytes / 1KB),@("KB"," KiB")[$UseSI.IsPresent]}
+else                   {"{0} bytes" -f  $bytes}
--- Get-NetFrameworkVersions.ps1
+++ Get-NetFrameworkVersions.ps1
@@ -14,18 +14,19 @@
 [Alias('Server')][string]$ComputerName = $env:COMPUTERNAME
 )
 
-function ConvertTo-ComplexVersion([string]$Name,[string]$Version)
-{New-Object psobject -Property ([ordered]@{Name=$Name;Version=[version]$Version})}
+function Add-Version([string]$Name,[string]$Version)
+{if($Version){@{$Name=[version]$Version}}else{@{}}}
 
 $hklm = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine',$ComputerName)
 $ndp = $hklm.OpenSubKey('SOFTWARE\Microsoft\NET Framework Setup\NDP')
 
+$versions = @{}
 # get v1-v3.x
 $ndp.GetSubKeyNames() |
     ? {$_ -like 'v[123].*'} |
     % {
         $k = $ndp.OpenSubKey($_)
-        ConvertTo-ComplexVersion $_ $k.GetValue('Version')
+        $versions += Add-Version $_ $k.GetValue('Version')
         [void]$k.Dispose(); $k = $null
     }
 
@@ -35,23 +36,23 @@ try
 {
     switch($v4.GetValue('Release'))
     { # see https://msdn.microsoft.com/en-us/library/hh925568.aspx
-        378389 {ConvertTo-ComplexVersion 'v4.5' $v4.GetValue('Version')}
-        378675 {ConvertTo-ComplexVersion 'v4.5.1' $v4.GetValue('Version')}
-        378758 {ConvertTo-ComplexVersion 'v4.5.1' $v4.GetValue('Version')}
-        379893 {ConvertTo-ComplexVersion 'v4.5.2' $v4.GetValue('Version')}
-        393295 {ConvertTo-ComplexVersion 'v4.6' $v4.GetValue('Version')}
-        393297 {ConvertTo-ComplexVersion 'v4.6' $v4.GetValue('Version')}
+        378389 {$versions += Add-Version 'v4.5' $v4.GetValue('Version')}
+        378675 {$versions += Add-Version 'v4.5.1' $v4.GetValue('Version')}
+        378758 {$versions += Add-Version 'v4.5.1' $v4.GetValue('Version')}
+        379893 {$versions += Add-Version 'v4.5.2' $v4.GetValue('Version')}
+        393295 {$versions += Add-Version 'v4.6' $v4.GetValue('Version')}
+        393297 {$versions += Add-Version 'v4.6' $v4.GetValue('Version')}
     }    
 }
 catch [ArgumentNullException]
 {
-    "Unable to open the version sub key"
+    Write-Warning "Unable to open the version 4 sub key."
 }
 finally
 {
-    if(!!$v4) {[void]$v4.Dispose(); $v4 = $null}
+    if($v4) {[void]$v4.Dispose(); $v4 = $null}
     [void]$hklm.Dispose(); $hklm = $null
     [void]$ndp.Dispose(); $ndp = $null
 }
 
-
+$versions
