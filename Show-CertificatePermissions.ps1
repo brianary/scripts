788e0f044a8f7c65690d2d3f822c1f9ab31bb70a
Brian Lalonde <brianl@stcu.org>
2015-09-25 14:13:17 -0700

    Certificate and app pool cert manipulation.

--- Show-CertificatePermissions.ps1
+++ Show-CertificatePermissions.ps1
@@ -9,12 +9,25 @@
     Find-Certificate.ps1
 
 .Example
+    Show-CertificatePermissions.ps1 -Certificate $cert
+    Displays the permissions for the certificate in $cert.
+
+.Example
     Find-Certificate.ps1 -FindValue ExampleCert -FindType FindBySubjectName -StoreName TrustedPeople -StoreLocation LocalMachine |Show-CertificatePermissions
     Displays the permissions for the certificate.
+
+.Example
+    $c = Find-Certificate.ps1 ExampleCert FindBySubjectName TrustedPeople LocalMachine ; Show-CertificatePermissions.ps1 $c
+    Another approach to display cert permissions.
 #>
 
 [CmdletBinding()] Param(
 [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)]
 [System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate
 )
-ls "$env:ProgramData\Microsoft\Crypto\RSA\MachineKeys","$env:APPDATA\Microsoft\Crypto\RSA" -Recurse -Filter $cert.PrivateKey.CspKeyContainerInfo.UniqueKeyContainerName |% FullName |% {icacls $_}
+Process
+{
+    "$env:ProgramData\Microsoft\Crypto\RSA\MachineKeys","$env:APPDATA\Microsoft\Crypto\RSA" |
+        ls -Recurse -Filter $Certificate.PrivateKey.CspKeyContainerInfo.UniqueKeyContainerName -EA SilentlyContinue |
+        % {icacls $_.FullName}
+}
