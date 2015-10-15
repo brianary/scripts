89ba942d6e6f4b74690dd0f66e54dca75cae8f9e
Brian Lalonde <brianl@stcu.org>
2015-10-14 17:14:05 -0700

    Reconfigure param processing, support literal char param.

--- Get-CharacterDetails.ps1
+++ Get-CharacterDetails.ps1
@@ -11,10 +11,8 @@ A specific Unicode block (or named range) of characters to return.
 
 #requires -version 2
 [CmdletBinding()] Param(
-[Parameter(ParameterSetName='Range',Position=0)][int]$StartValue = [char]::MinValue,
-[Parameter(ParameterSetName='Range',Position=1)][int]$StopValue  = [char]::MaxValue,
-[Parameter(ParameterSetName='Block',Position=0,Mandatory=$true)]
-[ValidateSet('ASCII','0x2xxx','BasicLatin','Latin1Supplement',
+[Parameter(ParameterSetName='Block',Position=0)]
+[ValidateSet('BasicMultilingualPlane','BMP','ASCII','0x2xxx','BasicLatin','Latin1Supplement',
 'LatinExtendedA','LatinExtendedB','IPAExtensions','SpacingModifierLetters','CombiningDiacriticalMarks',
 'GreekandCoptic','Cyrillic','CyrillicSupplement','Armenian','Hebrew','Arabic','Syriac','ArabicSupplement',
 'Thaana','NKo','Samaritan','Mandaic','ArabicExtendedA','Devanagari','Bengali','Gurmukhi','Gujarati','Oriya',
@@ -41,7 +39,10 @@ A specific Unicode block (or named range) of characters to return.
 'HangulJamoExtendedB','HighSurrogates','HighPrivateUseSurrogates','LowSurrogates','PrivateUseArea',
 'CJKCompatibilityIdeographs','AlphabeticPresentationForms','ArabicPresentationFormsA','VariationSelectors',
 'VerticalForms','CombiningHalfMarks','CJKCompatibilityForms','SmallFormVariants','ArabicPresentationFormsB',
-'HalfwidthandFullwidthForms','Specials')][string]$Block,
+'HalfwidthandFullwidthForms','Specials')][string]$Block = 'BasicMultilingualPlane',
+[Parameter(ParameterSetName='Char',Mandatory=$true)][string]$Char,
+[Parameter(ParameterSetName='Range',Position=0,Mandatory=$true)][int]$StartValue,
+[Parameter(ParameterSetName='Range',Position=1,Mandatory=$true)][int]$StopValue,
 [switch]$IsControl,      [switch]$NotControl,
 [switch]$IsDigit,        [switch]$NotDigit,
 [switch]$IsHighSurrogate,[switch]$NotHighSurrogate,
@@ -60,170 +61,175 @@ A specific Unicode block (or named range) of characters to return.
 [switch]$IsUpper,        [switch]$NotUpper,
 [switch]$IsWhiteSpace,   [switch]$NotWhiteSpace
 )
-($StartValue,$StopValue) = switch($Block)
+if(!$StopValue)
 {
-    ASCII {0x0000,0x007F}
-    0x2xxx {0x2000,0x2FFF}
-    BasicLatin {0x0000,0x007F}
-    Latin1Supplement {0x0080,0x00FF}
-    LatinExtendedA {0x0100,0x017F}
-    LatinExtendedB {0x0180,0x024F}
-    IPAExtensions {0x0250,0x02AF}
-    SpacingModifierLetters {0x02B0,0x02FF}
-    CombiningDiacriticalMarks {0x0300,0x036F}
-    GreekandCoptic {0x0370,0x03FF}
-    Cyrillic {0x0400,0x04FF}
-    CyrillicSupplement {0x0500,0x052F}
-    Armenian {0x0530,0x058F}
-    Hebrew {0x0590,0x05FF}
-    Arabic {0x0600,0x06FF}
-    Syriac {0x0700,0x074F}
-    ArabicSupplement {0x0750,0x077F}
-    Thaana {0x0780,0x07BF}
-    NKo {0x07C0,0x07FF}
-    Samaritan {0x0800,0x083F}
-    Mandaic {0x0840,0x085F}
-    ArabicExtendedA {0x08A0,0x08FF}
-    Devanagari {0x0900,0x097F}
-    Bengali {0x0980,0x09FF}
-    Gurmukhi {0x0A00,0x0A7F}
-    Gujarati {0x0A80,0x0AFF}
-    Oriya {0x0B00,0x0B7F}
-    Tamil {0x0B80,0x0BFF}
-    Telugu {0x0C00,0x0C7F}
-    Kannada {0x0C80,0x0CFF}
-    Malayalam {0x0D00,0x0D7F}
-    Sinhala {0x0D80,0x0DFF}
-    Thai {0x0E00,0x0E7F}
-    Lao {0x0E80,0x0EFF}
-    Tibetan {0x0F00,0x0FFF}
-    Myanmar {0x1000,0x109F}
-    Georgian {0x10A0,0x10FF}
-    HangulJamo {0x1100,0x11FF}
-    Ethiopic {0x1200,0x137F}
-    EthiopicSupplement {0x1380,0x139F}
-    Cherokee {0x13A0,0x13FF}
-    UnifiedCanadianAboriginalSyllabics {0x1400,0x167F}
-    Ogham {0x1680,0x169F}
-    Runic {0x16A0,0x16FF}
-    Tagalog {0x1700,0x171F}
-    Hanunoo {0x1720,0x173F}
-    Buhid {0x1740,0x175F}
-    Tagbanwa {0x1760,0x177F}
-    Khmer {0x1780,0x17FF}
-    Mongolian {0x1800,0x18AF}
-    UnifiedCanadianAboriginalSyllabicsExtended {0x18B0,0x18FF}
-    Limbu {0x1900,0x194F}
-    TaiLe {0x1950,0x197F}
-    NewTaiLue {0x1980,0x19DF}
-    KhmerSymbols {0x19E0,0x19FF}
-    Buginese {0x1A00,0x1A1F}
-    TaiTham {0x1A20,0x1AAF}
-    CombiningDiacriticalMarksExtended {0x1AB0,0x1AFF}
-    Balinese {0x1B00,0x1B7F}
-    Sundanese {0x1B80,0x1BBF}
-    Batak {0x1BC0,0x1BFF}
-    Lepcha {0x1C00,0x1C4F}
-    OlChiki {0x1C50,0x1C7F}
-    SundaneseSupplement {0x1CC0,0x1CCF}
-    VedicExtensions {0x1CD0,0x1CFF}
-    PhoneticExtensions {0x1D00,0x1D7F}
-    PhoneticExtensionsSupplement {0x1D80,0x1DBF}
-    CombiningDiacriticalMarksSupplement {0x1DC0,0x1DFF}
-    LatinExtendedAdditional {0x1E00,0x1EFF}
-    GreekExtended {0x1F00,0x1FFF}
-    GeneralPunctuation {0x2000,0x206F}
-    SuperscriptsandSubscripts {0x2070,0x209F}
-    CurrencySymbols {0x20A0,0x20CF}
-    CombiningDiacriticalMarksforSymbols {0x20D0,0x20FF}
-    LetterlikeSymbols {0x2100,0x214F}
-    NumberForms {0x2150,0x218F}
-    Arrows {0x2190,0x21FF}
-    MathematicalOperators {0x2200,0x22FF}
-    MiscellaneousTechnical {0x2300,0x23FF}
-    ControlPictures {0x2400,0x243F}
-    OpticalCharacterRecognition {0x2440,0x245F}
-    EnclosedAlphanumerics {0x2460,0x24FF}
-    BoxDrawing {0x2500,0x257F}
-    BlockElements {0x2580,0x259F}
-    GeometricShapes {0x25A0,0x25FF}
-    MiscellaneousSymbols {0x2600,0x26FF}
-    Dingbats {0x2700,0x27BF}
-    MiscellaneousMathematicalSymbolsA {0x27C0,0x27EF}
-    SupplementalArrowsA {0x27F0,0x27FF}
-    BraillePatterns {0x2800,0x28FF}
-    SupplementalArrowsB {0x2900,0x297F}
-    MiscellaneousMathematicalSymbolsB {0x2980,0x29FF}
-    SupplementalMathematicalOperators {0x2A00,0x2AFF}
-    MiscellaneousSymbolsandArrows {0x2B00,0x2BFF}
-    Glagolitic {0x2C00,0x2C5F}
-    LatinExtendedC {0x2C60,0x2C7F}
-    Coptic {0x2C80,0x2CFF}
-    GeorgianSupplement {0x2D00,0x2D2F}
-    Tifinagh {0x2D30,0x2D7F}
-    EthiopicExtended {0x2D80,0x2DDF}
-    CyrillicExtendedA {0x2DE0,0x2DFF}
-    SupplementalPunctuation {0x2E00,0x2E7F}
-    CJKRadicalsSupplement {0x2E80,0x2EFF}
-    KangxiRadicals {0x2F00,0x2FDF}
-    IdeographicDescriptionCharacters {0x2FF0,0x2FFF}
-    CJKSymbolsandPunctuation {0x3000,0x303F}
-    Hiragana {0x3040,0x309F}
-    Katakana {0x30A0,0x30FF}
-    Bopomofo {0x3100,0x312F}
-    HangulCompatibilityJamo {0x3130,0x318F}
-    Kanbun {0x3190,0x319F}
-    BopomofoExtended {0x31A0,0x31BF}
-    CJKStrokes {0x31C0,0x31EF}
-    KatakanaPhoneticExtensions {0x31F0,0x31FF}
-    EnclosedCJKLettersandMonths {0x3200,0x32FF}
-    CJKCompatibility {0x3300,0x33FF}
-    CJKUnifiedIdeographsExtensionA {0x3400,0x4DBF}
-    YijingHexagramSymbols {0x4DC0,0x4DFF}
-    CJKUnifiedIdeographs {0x4E00,0x9FFF}
-    YiSyllables {0xA000,0xA48F}
-    YiRadicals {0xA490,0xA4CF}
-    Lisu {0xA4D0,0xA4FF}
-    Vai {0xA500,0xA63F}
-    CyrillicExtendedB {0xA640,0xA69F}
-    Bamum {0xA6A0,0xA6FF}
-    ModifierToneLetters {0xA700,0xA71F}
-    LatinExtendedD {0xA720,0xA7FF}
-    SylotiNagri {0xA800,0xA82F}
-    CommonIndicNumberForms {0xA830,0xA83F}
-    Phagspa {0xA840,0xA87F}
-    Saurashtra {0xA880,0xA8DF}
-    DevanagariExtended {0xA8E0,0xA8FF}
-    KayahLi {0xA900,0xA92F}
-    Rejang {0xA930,0xA95F}
-    HangulJamoExtendedA {0xA960,0xA97F}
-    Javanese {0xA980,0xA9DF}
-    MyanmarExtendedB {0xA9E0,0xA9FF}
-    Cham {0xAA00,0xAA5F}
-    MyanmarExtendedA {0xAA60,0xAA7F}
-    TaiViet {0xAA80,0xAADF}
-    MeeteiMayekExtensions {0xAAE0,0xAAFF}
-    EthiopicExtendedA {0xAB00,0xAB2F}
-    LatinExtendedE {0xAB30,0xAB6F}
-    MeeteiMayek {0xABC0,0xABFF}
-    HangulSyllables {0xAC00,0xD7AF}
-    HangulJamoExtendedB {0xD7B0,0xD7FF}
-    HighSurrogates {0xD800,0xDB7F}
-    HighPrivateUseSurrogates {0xDB80,0xDBFF}
-    LowSurrogates {0xDC00,0xDFFF}
-    PrivateUseArea {0xE000,0xF8FF}
-    CJKCompatibilityIdeographs {0xF900,0xFAFF}
-    AlphabeticPresentationForms {0xFB00,0xFB4F}
-    ArabicPresentationFormsA {0xFB50,0xFDFF}
-    VariationSelectors {0xFE00,0xFE0F}
-    VerticalForms {0xFE10,0xFE1F}
-    CombiningHalfMarks {0xFE20,0xFE2F}
-    CJKCompatibilityForms {0xFE30,0xFE4F}
-    SmallFormVariants {0xFE50,0xFE6F}
-    ArabicPresentationFormsB {0xFE70,0xFEFF}
-    HalfwidthandFullwidthForms {0xFF00,0xFFEF}
-    Specials {0xFFF0,0xFFFF}
-    default {[char]::MinValue,[char]::MaxValue}
+    ($StartValue,$StopValue) = switch($Block)
+    {
+        BasicMultilingualPlane {[char]::MinValue,[char]::MaxValue}
+        BMP {[char]::MinValue,[char]::MaxValue}
+        ASCII {0x0000,0x007F}
+        0x2xxx {0x2000,0x2FFF}
+        BasicLatin {0x0000,0x007F}
+        Latin1Supplement {0x0080,0x00FF}
+        LatinExtendedA {0x0100,0x017F}
+        LatinExtendedB {0x0180,0x024F}
+        IPAExtensions {0x0250,0x02AF}
+        SpacingModifierLetters {0x02B0,0x02FF}
+        CombiningDiacriticalMarks {0x0300,0x036F}
+        GreekandCoptic {0x0370,0x03FF}
+        Cyrillic {0x0400,0x04FF}
+        CyrillicSupplement {0x0500,0x052F}
+        Armenian {0x0530,0x058F}
+        Hebrew {0x0590,0x05FF}
+        Arabic {0x0600,0x06FF}
+        Syriac {0x0700,0x074F}
+        ArabicSupplement {0x0750,0x077F}
+        Thaana {0x0780,0x07BF}
+        NKo {0x07C0,0x07FF}
+        Samaritan {0x0800,0x083F}
+        Mandaic {0x0840,0x085F}
+        ArabicExtendedA {0x08A0,0x08FF}
+        Devanagari {0x0900,0x097F}
+        Bengali {0x0980,0x09FF}
+        Gurmukhi {0x0A00,0x0A7F}
+        Gujarati {0x0A80,0x0AFF}
+        Oriya {0x0B00,0x0B7F}
+        Tamil {0x0B80,0x0BFF}
+        Telugu {0x0C00,0x0C7F}
+        Kannada {0x0C80,0x0CFF}
+        Malayalam {0x0D00,0x0D7F}
+        Sinhala {0x0D80,0x0DFF}
+        Thai {0x0E00,0x0E7F}
+        Lao {0x0E80,0x0EFF}
+        Tibetan {0x0F00,0x0FFF}
+        Myanmar {0x1000,0x109F}
+        Georgian {0x10A0,0x10FF}
+        HangulJamo {0x1100,0x11FF}
+        Ethiopic {0x1200,0x137F}
+        EthiopicSupplement {0x1380,0x139F}
+        Cherokee {0x13A0,0x13FF}
+        UnifiedCanadianAboriginalSyllabics {0x1400,0x167F}
+        Ogham {0x1680,0x169F}
+        Runic {0x16A0,0x16FF}
+        Tagalog {0x1700,0x171F}
+        Hanunoo {0x1720,0x173F}
+        Buhid {0x1740,0x175F}
+        Tagbanwa {0x1760,0x177F}
+        Khmer {0x1780,0x17FF}
+        Mongolian {0x1800,0x18AF}
+        UnifiedCanadianAboriginalSyllabicsExtended {0x18B0,0x18FF}
+        Limbu {0x1900,0x194F}
+        TaiLe {0x1950,0x197F}
+        NewTaiLue {0x1980,0x19DF}
+        KhmerSymbols {0x19E0,0x19FF}
+        Buginese {0x1A00,0x1A1F}
+        TaiTham {0x1A20,0x1AAF}
+        CombiningDiacriticalMarksExtended {0x1AB0,0x1AFF}
+        Balinese {0x1B00,0x1B7F}
+        Sundanese {0x1B80,0x1BBF}
+        Batak {0x1BC0,0x1BFF}
+        Lepcha {0x1C00,0x1C4F}
+        OlChiki {0x1C50,0x1C7F}
+        SundaneseSupplement {0x1CC0,0x1CCF}
+        VedicExtensions {0x1CD0,0x1CFF}
+        PhoneticExtensions {0x1D00,0x1D7F}
+        PhoneticExtensionsSupplement {0x1D80,0x1DBF}
+        CombiningDiacriticalMarksSupplement {0x1DC0,0x1DFF}
+        LatinExtendedAdditional {0x1E00,0x1EFF}
+        GreekExtended {0x1F00,0x1FFF}
+        GeneralPunctuation {0x2000,0x206F}
+        SuperscriptsandSubscripts {0x2070,0x209F}
+        CurrencySymbols {0x20A0,0x20CF}
+        CombiningDiacriticalMarksforSymbols {0x20D0,0x20FF}
+        LetterlikeSymbols {0x2100,0x214F}
+        NumberForms {0x2150,0x218F}
+        Arrows {0x2190,0x21FF}
+        MathematicalOperators {0x2200,0x22FF}
+        MiscellaneousTechnical {0x2300,0x23FF}
+        ControlPictures {0x2400,0x243F}
+        OpticalCharacterRecognition {0x2440,0x245F}
+        EnclosedAlphanumerics {0x2460,0x24FF}
+        BoxDrawing {0x2500,0x257F}
+        BlockElements {0x2580,0x259F}
+        GeometricShapes {0x25A0,0x25FF}
+        MiscellaneousSymbols {0x2600,0x26FF}
+        Dingbats {0x2700,0x27BF}
+        MiscellaneousMathematicalSymbolsA {0x27C0,0x27EF}
+        SupplementalArrowsA {0x27F0,0x27FF}
+        BraillePatterns {0x2800,0x28FF}
+        SupplementalArrowsB {0x2900,0x297F}
+        MiscellaneousMathematicalSymbolsB {0x2980,0x29FF}
+        SupplementalMathematicalOperators {0x2A00,0x2AFF}
+        MiscellaneousSymbolsandArrows {0x2B00,0x2BFF}
+        Glagolitic {0x2C00,0x2C5F}
+        LatinExtendedC {0x2C60,0x2C7F}
+        Coptic {0x2C80,0x2CFF}
+        GeorgianSupplement {0x2D00,0x2D2F}
+        Tifinagh {0x2D30,0x2D7F}
+        EthiopicExtended {0x2D80,0x2DDF}
+        CyrillicExtendedA {0x2DE0,0x2DFF}
+        SupplementalPunctuation {0x2E00,0x2E7F}
+        CJKRadicalsSupplement {0x2E80,0x2EFF}
+        KangxiRadicals {0x2F00,0x2FDF}
+        IdeographicDescriptionCharacters {0x2FF0,0x2FFF}
+        CJKSymbolsandPunctuation {0x3000,0x303F}
+        Hiragana {0x3040,0x309F}
+        Katakana {0x30A0,0x30FF}
+        Bopomofo {0x3100,0x312F}
+        HangulCompatibilityJamo {0x3130,0x318F}
+        Kanbun {0x3190,0x319F}
+        BopomofoExtended {0x31A0,0x31BF}
+        CJKStrokes {0x31C0,0x31EF}
+        KatakanaPhoneticExtensions {0x31F0,0x31FF}
+        EnclosedCJKLettersandMonths {0x3200,0x32FF}
+        CJKCompatibility {0x3300,0x33FF}
+        CJKUnifiedIdeographsExtensionA {0x3400,0x4DBF}
+        YijingHexagramSymbols {0x4DC0,0x4DFF}
+        CJKUnifiedIdeographs {0x4E00,0x9FFF}
+        YiSyllables {0xA000,0xA48F}
+        YiRadicals {0xA490,0xA4CF}
+        Lisu {0xA4D0,0xA4FF}
+        Vai {0xA500,0xA63F}
+        CyrillicExtendedB {0xA640,0xA69F}
+        Bamum {0xA6A0,0xA6FF}
+        ModifierToneLetters {0xA700,0xA71F}
+        LatinExtendedD {0xA720,0xA7FF}
+        SylotiNagri {0xA800,0xA82F}
+        CommonIndicNumberForms {0xA830,0xA83F}
+        Phagspa {0xA840,0xA87F}
+        Saurashtra {0xA880,0xA8DF}
+        DevanagariExtended {0xA8E0,0xA8FF}
+        KayahLi {0xA900,0xA92F}
+        Rejang {0xA930,0xA95F}
+        HangulJamoExtendedA {0xA960,0xA97F}
+        Javanese {0xA980,0xA9DF}
+        MyanmarExtendedB {0xA9E0,0xA9FF}
+        Cham {0xAA00,0xAA5F}
+        MyanmarExtendedA {0xAA60,0xAA7F}
+        TaiViet {0xAA80,0xAADF}
+        MeeteiMayekExtensions {0xAAE0,0xAAFF}
+        EthiopicExtendedA {0xAB00,0xAB2F}
+        LatinExtendedE {0xAB30,0xAB6F}
+        MeeteiMayek {0xABC0,0xABFF}
+        HangulSyllables {0xAC00,0xD7AF}
+        HangulJamoExtendedB {0xD7B0,0xD7FF}
+        HighSurrogates {0xD800,0xDB7F}
+        HighPrivateUseSurrogates {0xDB80,0xDBFF}
+        LowSurrogates {0xDC00,0xDFFF}
+        PrivateUseArea {0xE000,0xF8FF}
+        CJKCompatibilityIdeographs {0xF900,0xFAFF}
+        AlphabeticPresentationForms {0xFB00,0xFB4F}
+        ArabicPresentationFormsA {0xFB50,0xFDFF}
+        VariationSelectors {0xFE00,0xFE0F}
+        VerticalForms {0xFE10,0xFE1F}
+        CombiningHalfMarks {0xFE20,0xFE2F}
+        CJKCompatibilityForms {0xFE30,0xFE4F}
+        SmallFormVariants {0xFE50,0xFE6F}
+        ArabicPresentationFormsB {0xFE70,0xFEFF}
+        HalfwidthandFullwidthForms {0xFF00,0xFFEF}
+        Specials {0xFFF0,0xFFFF}
+        default {[char]::MinValue,[char]::MaxValue}
+    }
 }
 $invalidUserNameChars = '"/\[]:;|=,+*?<>'.ToCharArray() # https://technet.microsoft.com/en-us/library/bb726984.aspx
 function Get-UnicodeRangeBlock([int]$c)
@@ -449,9 +455,8 @@ MeeteiMayek
 HangulJamoExtended-B
 VerticalForms
 '@ -split '\s+'
-for($_ = $StartValue; $_ -le $StopValue; $_++)
+function Get-CharacterDetail([char]$c)
 {
-    [char]$c = $_
     $properties = [ordered]@{
         Character       = $c
         Value           = $_
@@ -511,9 +516,18 @@ for($_ = $StartValue; $_ -le $StopValue; $_++)
         ($NotUpper -and $properties.IsUpper) -or
         ($IsWhiteSpace -and !$properties.IsWhiteSpace) -or
         ($NotWhiteSpace -and $properties.IsWhiteSpace) )
-        {continue}
+        {return}
     $b = Get-UnicodeRangeBlock $_
     $properties.UnicodeBlock = $b
     $properties.MatchesBlock = if($notablock -contains $b) {'Error'} else {$c -match "\p{Is$b}"}
     New-Object PSObject -Property $properties
+}
+
+if($Char)
+{
+    foreach($c in $Char.GetEnumerator()) {Get-CharacterDetail $c}
+}
+else
+{
+    for($_ = $StartValue; $_ -le $StopValue; $_++) {[char]$c = $_; Get-CharacterDetail $c}
 }
