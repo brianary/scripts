7ee7779b90c6d205d3e97ef5af13725919e8cc5e
Brian Lalonde <brianary@gmail.com>
2015-10-19 10:50:16 -0700

    Test whether text is well-formed XML.

---
+++ Test-Xml.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.Synopsis
+    Try parsing text as XML.
+
+.Parameter Xml
+    The string to check.
+
+.Parameter Path
+    A file to check.
+
+.Parameter ErrorMessage
+    Instead of a boolean value, returns the parse error message, or nothing if successful.
+#>
+
+[CmdletBinding()] Param(
+[Parameter(ParameterSetName='Xml',Position=0,Mandatory=$true,ValueFromPipeline=$true)]
+[ValidateScript({!(Test-Path $_ -PathType Leaf)})][string] $Xml,
+[Parameter(ParameterSetName='Path',Mandatory=$true,ValueFromPipeline=$true)]
+[ValidateScript({Test-Path $_ -PathType Leaf})][string] $Path,
+[switch]$ErrorMessage
+)
+if($Path){$Xml= Get-Content $Path -Raw}
+try{[void][xml]$Xml; return $(if(!$ErrorMessage){$true})}
+catch [Management.Automation.RuntimeException]
+{
+    if(!$ErrorMessage){return $false}
+    else{return $_.Exception.InnerException.InnerException.Message}
+}
