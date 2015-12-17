0ddce2cd37587e1e137f3a63cb628552faea0142
Brian Lalonde <brianl@stcu.org>
2015-12-17 09:25:15 -0800

    Get cert path script; require finding cert; grant cert access to users.

--- Find-Certificate.ps1
+++ Find-Certificate.ps1
@@ -19,14 +19,17 @@
 .Parameter Current
     Whether to further filter search results by checking the effective and expiration dates.
 
-.Link
-    https://msdn.microsoft.com/en-us/library/system.security.cryptography.x509certificates.x509findtype.aspx
+.Parameter Require
+    Whether to throw an error if a certificate is not found.
 
 .Link
-    https://msdn.microsoft.com/en-us/library/ms148581.aspx
+    https://msdn.microsoft.com/library/system.security.cryptography.x509certificates.x509findtype.aspx
 
 .Link
-    https://msdn.microsoft.com/en-us/library/system.security.cryptography.x509certificates.x509certificate2.aspx
+    https://msdn.microsoft.com/library/ms148581.aspx
+
+.Link
+    https://msdn.microsoft.com/library/system.security.cryptography.x509certificates.x509certificate2.aspx
 
 .Example
     Find-Certificate.ps1 -FindValue ExampleCert -FindType FindBySubjectName -StoreName TrustedPeople -StoreLocation LocalMachine
@@ -43,12 +46,13 @@
 [Parameter(Position=1,Mandatory=$true)][Alias('Type','Field')][Security.Cryptography.X509Certificates.X509FindType]$FindType,
 [Parameter(Position=2,Mandatory=$true)][Security.Cryptography.X509Certificates.StoreName]$StoreName,
 [Parameter(Position=3,Mandatory=$true)][Security.Cryptography.X509Certificates.StoreLocation]$StoreLocation,
-[switch]$Current
+[switch]$Current,
+[switch]$Require
 )
 $store = New-Object Security.Cryptography.X509Certificates.X509Store $StoreName,$StoreLocation
 [void]$store.Open('OpenExistingOnly')
 $cert = $store.Certificates.Find($FindType,$FindValue,$true)
 [void]$store.Close()
 $store = $null
-if(!$Current) { return $cert }
-else { $now = [DateTime]::Now ; return $cert |where NotAfter -gt $now |where NotBefore -lt $now }
+if($Current) { $now = [DateTime]::Now ; $cert = $cert |where NotAfter -gt $now |where NotBefore -lt $now }
+if($Require -and !$cert) {throw "Could not find certificate $FindType $FindValue in $StoreLocation $StoreName"}
