c1a6b28d11459559e4413ae0dcea852b08576f90
Brian Lalonde <brianary@gmail.com>
2016-05-24 16:43:39 -0700

    Add script to pull named regex matches into current variable namespace.

---
+++ Use-NamedMatches.ps1
@@ -0,0 +1,8 @@
+﻿<#
+.Synopsis
+    Creates local variables from named matches in $Matches.
+#>
+
+#requires -version 3
+[CmdletBinding()] Param()
+$Matches.Keys |? {$_ -isnot [int]} |% {Set-Variable $_ $Matches.$_ -Scope 1}
