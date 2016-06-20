3e83e79cbdc190e658ce4791382d719329aec057
Brian Lalonde <brianary@gmail.com>
2016-06-20 10:00:21 -0700

    Add user & process interactivity checker.

---
+++ Test-Interactive.ps1
@@ -0,0 +1,9 @@
+﻿<#
+.Synopsis
+    Determines whether both the user and process are interactive.
+#>
+
+#requires -Version 3
+[CmdletBinding()] Param()
+[Environment]::UserInteractive -and
+    !([Environment]::GetCommandLineArgs() |? {$_ -ilike '-NonI*'})
