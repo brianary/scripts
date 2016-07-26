f3493cae1cbde27ab87d1f06c1269f30c3e878c7
Brian Lalonde <brianary@gmail.com>
2016-07-26 14:40:56 -0700

    Count which indent chars used for each line.

---
+++ Measure-Indents.ps1
@@ -0,0 +1,47 @@
+﻿<#
+.Synopsis
+    Measures the indentation characters used in a text file.
+
+.Parameter Path
+    A file to measure.
+
+.Example
+    Measure-Indents.ps1 Program.cs
+
+
+    Tab Space Mix Other
+    --- ----- --- -----
+      1    17   0     0
+#>
+
+#requires -version 3
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][string]$Path
+)
+Begin
+{
+    $Count = New-Object psobject -Property @{Tab=0;Space=0;Mix=0;Other=0}
+}
+Process
+{
+    foreach($line in (Get-Content $Path))
+    {
+        if($line -notmatch '^(?<Indent>\s+)') {continue}
+        $IndentChars = $Matches.Indent.ToCharArray() |select -Unique
+        if($IndentChars -is [object[]]) {$Count.Mix++}
+        elseif($IndentChars -is [char])
+        {
+            switch($IndentChars)
+            {
+                "`t"    {$Count.Tab++}
+                ' '     {$Count.Space++}
+                default {$Count.Other++}
+            }
+        }
+        else {Write-Warning "Unexpected indent type: $($IndentChars.GetType().FullName)"}
+    }
+}
+End
+{
+    $Count
+}
