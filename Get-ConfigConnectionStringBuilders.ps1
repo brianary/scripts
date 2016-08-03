77735ce3c9b340eed66e3bbd35efe5543b05e6aa
Brian Lalonde <brianary@gmail.com>
2016-08-03 16:50:35 -0700

    Config file connection string parsing script

---
+++ Get-ConfigConnectionStringBuilders.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.Synopsis
+    Return named connection string builders for connection strings in a config file.
+
+#>
+
+#requires -version 3
+[CmdletBinding()] Param(
+[Parameter(Position=0,ValueFromPipelineByPropertyName=$true)][string]$Path
+)
+Process
+{
+    Select-Xml '//connectionStrings/add' $Path |
+        % { 
+            $provider = $_.Node.Attributes.GetNamedItem('providerName')
+            if($provider){$provider=$provider.Value}
+            New-Object psobject -Property ([ordered]@{
+                Name = $_.Node.name
+                ConnectionString = New-DbProviderObject.ps1 $provider ConnectionStringBuilder $_.Node.connectionString
+            })
+        }
+}
