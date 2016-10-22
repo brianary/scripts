956d05f66228b80cd99bdf6b312d3a8a36e46213
Brian Lalonde <brianary@gmail.com>
2016-10-22 15:21:23 -0700

    Add file backup and more persistent remove

---
+++ Backup-File.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.Synopsis
+    Create a backup as a sibling to a file, with date and time values in the name.
+
+.Parameter Path
+    Specifies a path to the items being removed. Wildcards are permitted.
+    The parameter name ("-Path") is optional.
+
+.Example
+    Backup-File.ps1 logfile.log
+
+
+    Copies logfile.log to logfile-201612311159.log (on that date & time).
+#>
+
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,ValueFromRemainingArguments=$true)]
+[string]$Path
+)
+$name = Split-Path $Path -Leaf
+$name = "$([IO.Path]::GetFileNameWithoutExtension($name))-$(Get-Date -Format yyyyMMddHHmmss).$([IO.Path]::GetExtension($name))"
+Copy-Item $Path (Join-Path (Split-Path $Path) $name)
---
+++ Remove-LockyFile.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.Synopsis
+    Removes a file that may be prone to locking.
+
+.Parameter Path
+    Specifies a path to the items being removed. Wildcards are permitted.
+    The parameter name ("-Path") is optional.
+
+.Example
+    Remove-LockyFile.ps1 InUse.dll
+
+
+    (Tries to remove file, renames it if unable to.)
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,ValueFromRemainingArguments=$true)]
+[string]$Path
+)
+Process
+{
+    try { Remove-Item $Path -Force }
+    catch
+    {
+        $NewPath = "$Path~$([guid]::NewGuid().Guid)"
+        Move-Item $Path $NewPath
+        try { Remove-Item $NewPath -Force }
+        catch { Write-Warning "Renamed file, but unable to remove $(Resolve-Path $NewPath)" }
+    }
+}
