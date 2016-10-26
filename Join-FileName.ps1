328916a0bc651c6feee3c8364d7de920032e3a93
Brian Lalonde <brianary@gmail.com>
2016-10-26 16:27:32 -0700

    Add filename manipulation and US holidays.

---
+++ Join-FileName.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.Synopsis
+    Combines a filename with a string.
+
+.Description
+    Join-FileName appends a string to a filename, including a new extension 
+    overwrites the filename's extension.
+
+.Example
+    Join-FileName.ps1 activity.log '-20161111'
+
+
+    activity-20161111.log
+
+.Example
+    Join-FileName.ps1 readme.txt .bak
+
+
+    readme.bak
+
+.Example
+    Join-FileName.ps1 C:\temp\activity.log .27.old
+
+
+    C:\temp\activity.27.old
+#>
+
+[CmdletBinding()] Param(
+[Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)][string] $Path,
+[Parameter(Mandatory=$true,Position=1)][Alias('Extension')][string] $AppendText
+)
+Process
+{
+	if($AppendText -like '.*') { [IO.Path]::ChangeExtension($Path,$AppendText) }
+	else
+	{
+		$name = [IO.Path]::GetFileNameWithoutExtension($Path) + $AppendText;
+		if($AppendText -notlike '*.*'){$name+=[IO.Path]::GetExtension($Path)}
+		if(Split-Path $Path) { Join-Path (Split-Path $Path) $name }
+		else { $name }
+	}
+}
---
+++ Test-USFederalHoliday.ps1
@@ -0,0 +1,96 @@
+﻿<#
+.Synopsis
+    Returns the name of the holiday of a date, if it is a U.S. federal holiday.
+
+.Description
+    The following holidays are checked:
+        * New Year's Day, January 1 (± 1 day, if observed)
+        * Martin Luther King, Jr. Day, Third Monday in January
+        * President's Day, Third Monday in February
+        * Memorial Day, Last Monday in May
+        * Independence Day, July 4 (± 1 day, if observed)
+        * Labor Day, First Monday in September
+        * Columbus Day, Second Monday in October
+        * Veteran's Day, November 11 (±1 day, if observed)
+        * Thanksgiving Day, Fourth Thursday in November
+        * Christmas Day, December 25 (±1 day, if observed)
+
+.Link
+    http://www.federalreserve.gov/aboutthefed/k8.htm
+
+.Example
+    Test-USFederalHoliday.ps1 2016-11-11
+
+
+    Veterans Day
+
+.Example
+    Test-USFederalHoliday.ps1 2017-02-20
+
+
+    Washington's Birthday
+
+.Example
+    if(!(Test-USFederalHoliday.ps1 ([datetime]::Today))) { Invoke-SomeBusinessDayTask }
+
+
+    Runs Invoke-SomeBusinessDayTask only if today is not a holiday.
+#>
+
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][datetime]$Date,
+[Parameter(HelpMessage='Are Saturday holidays observed on Friday?')][switch]$SatToFri,
+[Parameter(HelpMessage='Are Sunday holidays observed on Monday?')][switch]$SunToMon
+)
+Process
+{
+    $MMdd= '{0:MMdd}' -f $Date
+    switch($Date.DayOfWeek)
+    {
+        Monday
+        {
+            switch -regex ($MMdd)
+            {
+                '^01(?:1[5-9]|2[01])$' {return 'Birthday of Martin Luther King, Jr.'}
+                '^02(?:1[5-9]|2[01])$' {return 'Washington''s Birthday'}
+                '^05(?:2[5-9]|3[01])$' {return 'Memorial Day'}
+                '^090[1-7]$' {return 'Labor Day'}
+                '^10(?:0[89]|1[01-4])$' {return 'Columbus Day'}
+            }
+            if($SunToMon)
+            {
+                switch($MMdd)
+                {
+                    '0102' {return 'New Year''s Day (Observed)'}
+                    '0705' {return 'Independence Day (Observed)'}
+                    '1112' {return 'Veterans Day (Observed)'}
+                    '1226' {return 'Christmas Day (Observed)'}
+                }
+            }
+        }
+        Thursday
+        {
+            if($MMdd -match '^112[2-8]$') {return 'Thanksgiving Day'}
+        }
+        Friday
+        {
+            if($SatToFri)
+            {
+                switch($MMdd)
+                {
+                    '1231' {return 'New Year''s Day (Observed)'}
+                    '0703' {return 'Independence Day (Observed)'}
+                    '1110' {return 'Veteran''s Day (Observed)'}
+                    '1224' {return 'Christmas Day (Observed)'}
+                }
+            }
+        }
+    }
+    switch($MMdd)
+    {
+        '0101' {return 'New Year''s Day'}
+        '0704' {return 'Independence Day'}
+        '1111' {return 'Veteran''s Day'}
+        '1225' {return 'Christmas Day'}
+    }
+}
