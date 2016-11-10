33e68671bdfb6b7f3abd6c21e5d0f5f52db12060
Brian Lalonde <brianary@gmail.com>
2016-11-10 11:48:59 -0800

    Add support for query defaults.

--- Use-SqlcmdParams.ps1
+++ Use-SqlcmdParams.ps1
@@ -22,7 +22,18 @@
 #>
 
 #Requires -Version 3
-[CmdletBinding()] Param()
+[CmdletBinding()] Param(
+[string]$HostName,
+[int]$QueryTimeout,
+[int]$ConnectionTimeout,
+[int]$ErrorLevel,
+[int]$SeverityLevel,
+[int]$MaxCharLength,
+[int]$MaxBinaryLength,
+[switch]$DisableVariables,
+[switch]$DisableCommands,
+[switch]$EncryptConnection
+)
 $params = Get-Variable -Scope 1 -Name PSBoundParameters -ValueOnly -EA SilentlyContinue
 Import-Variables.ps1 $params
 $caller = Get-Variable -Scope 1 -Name PSCmdlet -ValueOnly -EA SilentlyContinue
@@ -37,6 +48,12 @@ $value =
             @{'Invoke-Sqlcmd:ConnectionString'=[Configuration.ConfigurationManager]::ConnectionStrings[$ConnectionName].ConnectionString}
         }
     }
+foreach($param in 'HostName','QueryTimeout','ConnectionTimeout','ErrorLevel','SeverityLevel','MaxCharLength','MaxBinaryLength','DisableVariables','DisableCommands','EncryptConnection')
+{
+    if($val = Get-Variable $param -ValueOnly -EA SilentlyContinue) {$value.Add($param,$val)}
+    elseif($val = Get-Variable $param -Scope 1 -ValueOnly -EA SilentlyContinue) {$value.Add($param,$val)}
+}
+Write-Verbose "Params: $(ConvertTo-Json $value -Compress)"
 $defaults = Get-Variable -Scope 1 -Name PSDefaultParameterValues -EA SilentlyContinue
 if($defaults) {$value.Keys |? {$value.$_ -and $defaults.Value.Contains($_)} |% {$defaults.Value.Remove($_)}; $defaults.Value += $value}
 else {Set-Variable -Scope 1 -Name PSDefaultParameterValues -Value $value}
