347ca823d69d98ddc9373e8de8f3bc94c30e0693
Brian Lalonde <brianary@gmail.com>
2016-12-04 13:41:40 -0800

    Add script dependencies graph.

--- Export-Readme.ps1
+++ Export-Readme.ps1
@@ -15,6 +15,28 @@ function Format-PSScripts
     ls $PSScriptRoot\*.ps1 |% {Get-Help $_.FullName} |% {"- **$(Split-Path $_.Name -Leaf)**: $($_.Synopsis)"}
 }
 
+function Format-Dependencies
+{
+    if($PSScriptRoot -notin ($env:Path -split ';')) {$env:Path += ";$PSScriptRoot"}
+    @'
+digraph ScriptDependencies
+{
+    rankdir=RL
+    node [shape=note style=filled fontname="Lucida Console" fontcolor=azure fillcolor=mediumblue color=azure]
+    edge [color=goldenrod]
+'@
+    foreach($help in Get-Help *.ps1)
+    {
+        if($help.Name -notlike "$PSScriptRoot\*" -or !$help.relatedLinks) {continue}
+        $help.relatedLinks.navigationLink.linkText |
+            ? {$_ -like '*.ps1'} |
+            % {"    `"$(Split-Path $help.Name -Leaf)`" -> `"$_`" "}
+    }
+    @'
+}
+'@
+}
+
 function Format-Readme
 {$local:OFS="`n";@"
 Useful General-Purpose Scripts
@@ -23,9 +45,16 @@ This repo contains a collection of generally useful scripts (mostly Windows, mos
 
 PowerShell Scripts
 ------------------
+
+![script dependencies](dependencies.png)
+
 $(Format-PSScripts)
 
 <!-- generated $(Get-Date) -->
 "@}
 
-Format-Readme |Out-File $PSScriptRoot\README.md utf8
+Use-Command.ps1 dot ${env:ProgramFiles(x86)}\Graphviz*\bin\dot.exe -msi http://graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.msi
+Format-Dependencies |Out-File $PSScriptRoot\dependencies.gv -Encoding ascii
+dot -Tpng -o $PSScriptRoot\dependencies.png $PSScriptRoot\dependencies.gv
+rm dependencies.gv
+Format-Readme |Out-File $PSScriptRoot\README.md
