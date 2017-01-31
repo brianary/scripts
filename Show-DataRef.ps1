2c4cc2fdb8dc8de3cf468050f1ece313c5d22d40
Brian Lalonde <brianary@gmail.com>
2017-01-31 13:45:33 -0800

    Add XSD/WSDL transform script.

---
+++ Show-DataRef.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.Synopsis
+    Display an HTML view of an XML schema or WSDL using Saxon.
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param(
+[Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$SchemaFile
+)
+Begin { Use-Command.ps1 saxon 'C:\Program Files\Saxonica\Saxon*\bin\Transform.exe' -nupkg saxon-he -InstallDir 'C:\Program Files\Saxonica' }
+Process
+{
+    $css  = Join-Path $PSScriptRoot 'dataref.css'
+    $xslt = Join-Path $PSScriptRoot 'dataref.xslt'
+    $html = [IO.Path]::ChangeExtension($SchemaFile,'html')
+    Copy-Item $css .
+    saxon -s:$SchemaFile -xsl:$xslt -o:$html
+    Invoke-Item $html
+}
