f581f0a1f45cd00eab97c60fb48a9a9b2b4c9b8f
Brian Lalonde <brianary@gmail.com>
2017-06-13 13:46:30 -0700

    Support numerically aligning money

--- Format-HtmlDataTable.ps1
+++ Format-HtmlDataTable.ps1
@@ -1,9 +1,6 @@
 ﻿<#
 .Synopsis
-    Right-aligns numeric data in an HTML table for emailing, with some additional styling options.
-
-.Parameter Caption
-    The text of the title (HTML <caption> element) for the table.
+    Right-aligns numeric data in an HTML table for emailing, and optionally zebra-stripes &c.
 
 .Parameter OddRowBackground
     The background CSS value for odd rows.
@@ -23,7 +20,7 @@
 .Outputs
     System.String (data-formatted HTML)
 
-.Notes
+.Note
     Assumes only one <tr> element per string piped in, as produced by ConvertTo-Html.
 
 .Link
@@ -31,9 +28,6 @@
 
 .Link
     https://www.w3.org/Bugs/Public/show_bug.cgi?id=18026
-
-.Link
-    https://www.campaignmonitor.com/css/
 
 .Example
     Invoke-Sqlcmd "..." |ConvertFrom-DataRow.ps1 |ConvertTo-Html |Format-HtmlDataTable.ps1
@@ -57,13 +51,13 @@ Begin
 }
 Process
 {
-    $Html = $Html -replace '<td>(\(\p{Sc}?\d+(?:[., ]\d+)*\)|(?:[-−﹣－+＋±]\p{Sc}|[-−﹣－+＋±]?\p{Sc}?)\d+(?:[., ]\d+)*[\p{Sc}%‰‱]?)</td>','<td align="right">$1</td>'
+    $odd = !$odd
+    $Html = $Html -replace '<td>([-$]?\d+(?:,\d{3})*(?:\.\d+)?)</td>','<td align="right">$1</td>'
     if($Html -like '*<table>*')
     {
         if($Caption) {$Html = $Html -replace '<table>',"<table><caption $CaptionAttributes>$([Net.WebUtility]::HtmlEncode($Caption))</caption>"}
         if($TableAttributes) {$Html = $Html -replace '<table>',"<table $TableAttributes>"}
     }
-    $odd = !$odd
     if($odd -and $OddRowBackground) {$Html -replace '^<tr>',"<tr style=`"background:$OddRowBackground`">"}
     elseif(!$odd -and $EvenRowBackground) {$Html -replace '^<tr>',"<tr style=`"background:$EvenRowBackground`">"}
     else {$Html}
