17fef3e692d3fe3018f9e4df88935ed63eb93342
Brian Lalonde <brianary@gmail.com>
2017-07-10 14:17:34 -0700

    Update versions, more durable version check.

--- Get-NetFrameworkVersions.ps1
+++ Get-NetFrameworkVersions.ps1
@@ -36,23 +36,33 @@ $ndp.GetSubKeyNames() |
 $v4 = $ndp.OpenSubKey('v4\Full')
 try
 {
-    $release = $v4.GetValue('Release')
+    [string]$release = $v4.GetValue('Release')
     Write-Verbose "v4 release $release"
-    switch($release)
-    { # see https://msdn.microsoft.com/en-us/library/hh925568.aspx
-        378389 {$versions += Add-Version 'v4.5' $v4.GetValue('Version')}
-        378675 {$versions += Add-Version 'v4.5.1' $v4.GetValue('Version')}
-        378758 {$versions += Add-Version 'v4.5.1' $v4.GetValue('Version')}
-        379893 {$versions += Add-Version 'v4.5.2' $v4.GetValue('Version')}
-        393295 {$versions += Add-Version 'v4.6' $v4.GetValue('Version')}
-        393297 {$versions += Add-Version 'v4.6' $v4.GetValue('Version')}
-        394254 {$versions += Add-Version 'v4.6.1' $v4.GetValue('Version')}
-        394271 {$versions += Add-Version 'v4.6.1' $v4.GetValue('Version')}
-        394747 {$versions += Add-Version 'v4.6.2+preview' $v4.GetValue('Version')}
-        394748 {$versions += Add-Version 'v4.6.2+preview' $v4.GetValue('Version')}
-        394802 {$versions += Add-Version 'v4.6.2' $v4.GetValue('Version')}
-        460798 {$versions += Add-Version 'v4.7' $v4.GetValue('Version')}
-    }    
+    $name = [ordered]@{ # see https://msdn.microsoft.com/en-us/library/hh925568.aspx
+        '460806' = 'v4.7+after-4.7'
+        '460805' = 'v4.7+pre-win10cu'
+        '460798' = 'v4.7+win10cu'
+        '394806' = 'v4.6.2+pre-win10ann'
+        '394802' = 'v4.6.2+win10ann'
+        '394748' = 'v4.6.2-preview'
+        '394747' = 'v4.6.2-preview'
+        '394271' = 'v4.6.1+pre-win10'
+        '394254' = 'v4.6.1+win10'
+        '393297' = 'v4.6+pre-win10'
+        '393295' = 'v4.6+win10'
+        '379893' = 'v4.5.2'
+        '378758' = 'v4.5.1+pre-win8.1'
+        '378675' = 'v4.5.1+win8.1'
+        '378389' = 'v4.5'
+    }
+    foreach($key in $name.Keys)
+    {
+        if($release -ge $key)
+        {
+            $versions += Add-Version $name.$key $v4.GetValue('Version')
+            break
+        }
+    }
 }
 catch [ArgumentNullException]
 {
