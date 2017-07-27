8704b7e5509df4ab3c51529c40b2d360eb7e3f8f
Brian Lalonde <brianary@gmail.com>
2017-07-27 13:07:01 -0700

    Add sane default cmdlet values script

---
+++ Use-ReasonableDefaults.ps1
@@ -0,0 +1,43 @@
+﻿<#
+.Synopsis
+    Sets certain cmdlet parameter defaults to rational, useful values.
+
+.Example
+    Use-ReasonableDefault.ps1
+
+    Sets default values:
+        Out-File -Width ([int]::MaxValue)
+        Select-Xml -Namespace @{ a bunch of standard namespaces }
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param()
+$Global:PSDefaultParameterValues['Out-File:Width'] = [int]::MaxValue
+$Global:PSDefaultParameterValues['Select-Xml:Namespace'] = @{
+xhtml = 'http://www.w3.org/1999/xhtml' 
+xsl   = 'http://www.w3.org/1999/XSL/Transform' 
+fn    = 'http://www.w3.org/2005/xpath-functions' 
+xs    = 'http://www.w3.org/2001/XMLSchema' 
+err   = 'http://www.w3.org/2005/xqt-errors' 
+atom  = 'http://www.w3.org/2005/Atom'
+rss   = 'http://purl.org/rss/1.0/' 
+rdf   = 'http://www.w3.org/1999/02/22-rdf-syntax-ns' 
+rdfs  = 'http://www.w3.org/2000/01/rdf-schema' 
+xsd   = 'http://www.w3.org/2001/XMLSchema' 
+msb   = 'http://schemas.microsoft.com/developer/msbuild/2003'
+owl   = 'http://www.w3.org/2002/07/owl' 
+dc    = 'http://purl.org/dc/terms/' 
+foaf  = 'http://xmlns.com/foaf/0.1/' 
+vcard = 'http://www.w3.org/2006/vcard/ns' 
+dbp   = 'http://dbpedia.org/dbprop/' 
+geo   = 'http://www.geonames.org/ontology' 
+gr    = 'http://purl.org/goodrelations/v1' 
+media = 'http://search.yahoo.com/searchmonkey/media/' 
+cb    = 'http://cb.semsol.org/ns'
+ps    = 'http://hfs-phoenix/DataTypes'
+task  = 'http://schemas.microsoft.com/windows/2004/02/mit/task'
+bcp   = 'http://schemas.microsoft.com/sqlserver/2004/bulkload/format'
+xsi   = 'http://www.w3.org/2001/XMLSchema-instance'
+m     = 'http://schemas.microsoft.com/ado/2007/08/dataservices/metadata' # SharePoint
+z     = '#RowsetSchema'
+}
