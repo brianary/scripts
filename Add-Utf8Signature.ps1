a36cc318fbc55b68e15aa715669633139a717f44
Brian Lalonde <brianary@gmail.com>
2018-03-05 14:42:20 -0800

    Break out UTF8 BOM/SIG scripts

---
+++ Add-Utf8Signature.ps1
@@ -0,0 +1,33 @@
+<#
+.Synopsis
+    Adds the utf-8 signature (BOM) from a file.
+
+.Parameter Path
+    The file to add the utf-8 signature to.
+
+.Link
+    https://msdn.microsoft.com/library/s064f8w2aspx
+
+.Link
+    https://msdn.microsoft.com/library/ms143376aspx
+
+.Link
+    Get-Content
+
+.Example
+    Add-Utf8Signature.ps1 README.md
+
+    Adds the EF BB BF at the beginning of the file, warns if it isn't found.
+#>
+
+#Requires -Version 4
+[CmdletBinding()] Param(
+[Parameter(Position=0,ValueFromPipelineByPropertyName=$true)][Alias('FullName')][string]$Path
+)
+Process
+{
+    Write-Verbose "Adding utf-8 BOM/SIG from $Path"
+    if(Test-FileTypeMagicNumber.ps1 utf8 $Path){Write-Warning "A utf-8 signature was already found in $Path"; return}
+    # this will have to do until PS6 http://brianary.github.io/powershell-encoding.html
+    [IO.File]::WriteAllText((Resolve-Path $Path),(Get-Content $Path -Raw),(New-Object System.Text.UTF8Encoding $true))
+}
