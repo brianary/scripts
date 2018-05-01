4a5e6001ad87869d193ff932f202dbc295209ae2
Brian Lalonde <brianary@gmail.com>
2018-05-01 11:51:22 -0700

    Add support for certificates & post-script manual TODO list

--- Export-WebConfiguration.ps1
+++ Export-WebConfiguration.ps1
@@ -2,9 +2,8 @@
 .Synopsis
     Exports IIS websites, app pools, and web apps as an idempotent PowerShell script to recreate them.
 
-.Outputs
-    System.String, PowerShell strings to create and configure IIS websites, app pools, and web apps
-    not found, as configured on the local machine.
+.Parameter Path
+    File to write import script text to.
 
 .Link
     https://chocolatey.org/
@@ -86,9 +85,22 @@
 #>
 
 #Requires -Version 3
-#Requires -RunAsAdministrator
+##Requires -RunAsAdministrator
 #Requires -Module WebAdministration
-[CmdletBinding()] Param()
+[CmdletBinding()] Param(
+[string]$Path = "Import-${env:ComputerName}WebConfiguration.ps1",
+[Security.Cryptography.X509Certificates.X509Store[]] $Stores =
+    ((Get-Item 'Cert:\LocalMachine\My'), (Get-Item 'Cert:\LocalMachine\TrustedPeople')),
+[Security.Cryptography.X509Certificates.X509KeyStorageFlags] $X509KeyStorageFlags = 'Exportable,MachineKeySet,PersistKeySet'
+)
+
+try {[void][Web.Security.Membership]} catch {Add-Type -AN System.Web}
+
+function Test-Administrator
+{
+    ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).`
+        IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
 
 function Export-Header
 {
@@ -103,16 +115,112 @@ function Export-Header
 #Requires -Module WebAdministration
 [CmdletBinding(SupportsShouldProcess=`$true,ConfirmImpact='High')] Param()
 
-`$todofile = [io.path]::ChangeExtension(`$PSCommandPath,'md')
+`$todofile = [io.path]::ChangeExtension(`$PSCommandPath,'txt')
 @"
-Manual TODO List
-================
+Manual $env:ComputerName Web Configuration TODO List
+======================================================
 
 `"@ |Out-File `$todofile utf8
 function Write-Todo([string]`$message)
 {
     Write-Warning "TODO: `$message (see `$todofile)"
-    "1. `$message" |Add-Content `$todofile
+    "- [ ] `$message" |Add-Content `$todofile
+}
+"@
+}
+
+function Get-CertificateFriendliestName(
+    [Parameter(Position=0,ValueFromPipeline=$true)][Security.Cryptography.X509Certificates.X509Certificate2]$cert,
+    [switch]$WithExpiry)
+{Process{
+    if(!$cert.FriendlyName) {return "$($cert.Subject) expiring $($cert.NotAfter)"}
+    else {return "$($cert.FriendlyName) expiring $($cert.NotAfter)"}
+}}
+
+function Get-CertificateImportName([Parameter(Position=0)][Security.Cryptography.X509Certificates.X509Store]$store,
+    [Parameter(Position=1,ValueFromPipeline=$true)][Security.Cryptography.X509Certificates.X509Certificate2]$cert)
+{Process{
+    $basename = "Import-CertificateTo_$($store.Location)_$($store.Name)_"
+    if(!$cert.FriendlyName) {return "$basename$($cert.Thumbprint)"}
+    else {return "$basename$($cert.FriendlyName -replace '\W+','_')"}
+}}
+
+function Export-CertificateFrom([Parameter(Position=0)][Security.Cryptography.X509Certificates.X509Store]$store,
+[Parameter(Position=1)][decimal]$PercentDenominator,
+[Parameter(Position=2,ValueFromPipeline=$true)][Security.Cryptography.X509Certificates.X509Certificate2]$cert)
+{Begin{$i = 0} Process{
+    $location,$name,$secret,$percent,$certname,$Local:OFS =
+        $store.Location,$store.Name,[Web.Security.Membership]::GeneratePassword(40,12),
+        [math]::Floor($i++/$PercentDenominator),(Get-CertificateFriendliestName $cert),''
+    Write-Progress "Exporting certificates from Cert:\$location\$name" $certname -Current $cert.Subject -Percent $percent
+    $qcertname = "$($certname -replace "'","''")"
+    $action =
+        try
+        {@"
+`$store.Add((New-Object Security.Cryptography.X509Certificates.X509Certificate2 ([convert]::FromBase64String(@'
+$([convert]::ToBase64String($cert.Export('Pfx',$secret),'InsertLineBreaks'))
+'@)),'$($secret -replace "'","''")','$X509KeyStorageFlags'))
+"@}
+        catch
+        {
+            Write-Warning "Unable to export ${certname}: $_"
+            $err = "$_" -replace "'","''" -replace '[\r\n]+',''
+            "Write-Warning 'Could not import $qcertname because it did not export successfully: $err'"
+        }
+    @"
+
+function $(Get-CertificateImportName $store $cert)
+{
+    [CmdletBinding(SupportsShouldProcess=`$true,ConfirmImpact="High")] Param(
+    [Parameter(Position=0)][Security.Cryptography.X509Certificates.X509Store]`$store
+    )
+    if(!(`$PSCmdlet.ShouldProcess('$qcertname','import into Cert:\$location\$name'))){return}
+    Write-Progress 'Importing certificates into Cert:\$location\$name' '$qcertname' ``
+        -Current '$($cert.Subject -replace "'","''")' -Percent $percent
+    $action
+}
+"@
+}}
+
+function Get-StoreImportName([Parameter(ValueFromPipeline=$true)]
+    [Security.Cryptography.X509Certificates.X509Store]$store)
+{Process{
+    return "Import-CertificateTo_$($store.Location)_$($store.Name)"
+}}
+
+function Export-CertificatesFrom([Parameter(ValueFromPipeline=$true)]
+    [Security.Cryptography.X509Certificates.X509Store]$store)
+{Process{ # Export-PfxCertificate is easier, but a bit too new to be broadly useful
+    $location,$name,$Local:OFS = $store.Location,$store.Name,"`r`n    "
+    $certs = Get-ChildItem Cert:\$location\$name
+    $certs |Export-CertificateFrom $store ($certs.Length/100)
+    @"
+
+function $(Get-StoreImportName $store)
+{
+    [CmdletBinding(SupportsShouldProcess=`$true,ConfirmImpact="High")] Param()
+    `$store = Get-Item Cert:\$location\$name
+    if(!(`$PSCmdlet.ShouldProcess('Cert:\$location\$name server certificates','import'))){return}
+    `$store.Open('OpenExistingOnly, ReadWrite')
+    $($certs |Get-CertificateImportName $store |% {"$_ `$store"})
+    `$store.Close()
+    `$store.Dispose()
+    Write-Progress 'Importing certificates into Cert:\$location\$name' -Completed
+}
+"@
+    Write-Progress "Exporting certificates from Cert:\$location\$name" -Completed
+}}
+
+function Export-Certificates
+{
+    $Local:OFS = "`r`n    "
+    $Stores |Export-CertificatesFrom
+    @"
+
+function Import-Certificates
+{
+    [CmdletBinding(SupportsShouldProcess=`$true,ConfirmImpact="High")] Param()
+    $($Stores |Get-StoreImportName)
 }
 "@
 }
@@ -169,21 +277,21 @@ function Import-WebAppPools
     $i,$max = 0,($appPools.Count/100.)
     foreach($appPool in $appPools)
     {
-        $path = "IIS:\AppPools\$($appPool.Name)"
-        Write-Progress 'Exporting application pools' $path -PercentComplete ($i/$max)
+        $iispath = "IIS:\AppPools\$($appPool.Name)"
+        Write-Progress 'Exporting application pools' $iispath -PercentComplete ($i/$max)
         @"
-    Write-Progress 'Importing application pools' '$path' -PercentComplete $([int]($i++/$max))
-    if(!(Test-Path '$path'))
+    Write-Progress 'Importing application pools' '$iispath' -PercentComplete $([int]($i++/$max))
+    if(!(Test-Path '$iispath'))
     {New-WebAppPool '$($appPool.Name)'}
     else
     {Write-Verbose 'App pool $($appPool.Name) found'}
 "@
         if($appPool.managedRuntimeVersion -notin '','v4.0'){@"
-    Set-ItemProperty '$path' managedRuntimeVersion $($appPool.managedRuntimeVersion)
+    Set-ItemProperty '$iispath' managedRuntimeVersion $($appPool.managedRuntimeVersion)
 "@}
-        $processModel = Get-ItemProperty $path processModel
+        $processModel = Get-ItemProperty $iispath processModel
         if($processModel.identityType -ne 'ApplicationPoolIdentity'){@"
-    Set-ItemProperty '$path' processModel @{userName='$($processModel.userName)';identityType='$($processModel.identityType)'}
+    Set-ItemProperty '$iispath' processModel @{userName='$($processModel.userName)';identityType='$($processModel.identityType)'}
 "@}
     }
     Write-Progress 'Exporting application pools' -Completed
@@ -230,6 +338,14 @@ function Import-Websites
         {
             $protocol,$ipAddress,$port,$hostHeader =
                 $binding.protocol,$binding.bindingInformation -split ':',3
+            $certbinding = Get-ChildItem IIS:\SslBindings |
+                ? {$_.Sites -eq $name -and $protocol -eq 'https'} |
+                % {@"
+|% {
+        try{`$_.AddSslCertificate('$($_.Thumbprint)','$($_.Store)')}
+        catch{Write-Error "Unable to bind certificate to ${name}: `$_"; Write-Todo 'Bind certificate for $name'}
+    }
+"@}
             @"
     if(!(Get-WebBinding -Name '$name' -Protocol $protocol -IPAddress $ipAddress -Port $port -HostHeader '$hostHeader'))
     {@{
@@ -238,7 +354,7 @@ function Import-Websites
         IPAddress  = '$ipAddress'
         Port       = '$port'
         HostHeader = '$hostHeader'
-    } |% {New-WebBinding @_}}
+    } |% {New-WebBinding @_} $certbinding }
     else
     {Write-Verbose 'Website $($website.name) $protocol binding ${ipAddress}:${port}:${hostHeader} found'}
 "@
@@ -251,21 +367,6 @@ function Import-Websites
 "@
 }
 
-function Export-ServerCertificates
-{
-    @"
-function Import-ServerCertificates
-{
-    [CmdletBinding(SupportsShouldProcess=`$true,ConfirmImpact="High")] Param()
-    if(!(`$PSCmdlet.ShouldProcess('server certificates','import'))){return}
-"@
-    #TODO: gcm -mo pki
-    @"
-    Write-Progress 'Importing server certificates' -Completed
-}
-"@
-}
-
 function Get-LocationConfigPaths([string]$xpath)
 {
     Select-Xml "/configuration/location[system.webServer/$xpath]/@path" (Get-WebConfigFile) |% {$_.Node.Value}
@@ -273,46 +374,58 @@ function Get-LocationConfigPaths([string]$xpath)
 
 function Export-WebApplications
 {
+    $sitepath = @{}
+    Get-Website |% {$sitepath[$_.Name]=$_.PhysicalPath}
     [object[]]$webapps = Get-WebApplication
     $i,$max,$functions = 0,($webapps.Count/100.),@()
     foreach($webapp in $webapps)
     {
-        $site = $webapp.GetParentElement()['name']
-        $apppath = $webapp.path.Trim('/')
-        $path = "IIS:\Sites\$site\$apppath"
-        $funcname = "Import-WebApplication_$($site -replace '\W+','')_$($apppath -replace '\W+','')"
+        $name,$site,$physicalpath,$pool =
+            $webapp.path.Trim('/'),
+            $webapp.GetParentElement()['name'],
+            $webapp.PhysicalPath,
+            $webapp.applicationPool
+        $iispath = "IIS:\Sites\$site\$name"
+        if(!$physicalpath)
+        {
+            Write-Warning "Physical path for $iispath isn't accessible. Will have to guess. Try restarting PowerShell if this is failing for all apps."
+            $physicalpath = "$($sitepath[$site])\$name"
+            if(!(Test-Path $physicalpath -PathType Container))
+            { Write-Error "Guessed physical path for $iispath ($physicalpath) not found. Manual fix required." }
+        }
+        $funcname = "Import-WebApplication_$($site -replace '\W+','')_$($name -replace '\W+','')"
         $functions += $funcname
         @"
+
 function $funcname
 {
     [CmdletBinding(SupportsShouldProcess=`$true,ConfirmImpact="High")] Param()
-    if(!(`$PSCmdlet.ShouldProcess('$site web application: $apppath','import'))){return}
+    if(!(`$PSCmdlet.ShouldProcess('$site web application: $name','import'))){return}
 "@
         $local:PSDefaultParameterValues = @{
             'Get-WebConfiguration:PSPath'           = 'MACHINE/WEBROOT/APPHOST'
-            'Get-WebConfiguration:Location'         = "$site/$apppath"
+            'Get-WebConfiguration:Location'         = "$site/$name"
             'Get-WebConfigurationProperty:PSPath'   = 'MACHINE/WEBROOT/APPHOST'
-            'Get-WebConfigurationProperty:Location' = "$site/$apppath"
+            'Get-WebConfigurationProperty:Location' = "$site/$name"
         }
-        $cfgcontext = "-PSPath MACHINE/WEBROOT/APPHOST -Location '$site/$apppath'"
-        if(!($webapp.PhysicalPath)){throw "Physical paths on web applications aren't accessible. Try restarting PowerShell."}
-        Write-Progress 'Exporting web applications' $path -Current $webapp.applicationPool -Percent ($i/$max)
+        $cfgcontext = "-PSPath MACHINE/WEBROOT/APPHOST -Location '$site/$name'"
+        Write-Progress 'Exporting web applications' $iispath -Current $pool -Percent ($i/$max)
         @"
-    Write-Progress 'Importing web applications' '$path' -Current '$($webapp.applicationPool)' -Percent $([int]($i++/$max))
-    if(!(Get-WebApplication '$apppath' -Site '$site'))
+    Write-Progress 'Importing web applications' '$iispath' -Current '$pool' -Percent $([int]($i++/$max))
+    if(!(Get-WebApplication '$name' -Site '$site'))
     {@{
+        Name            = '$name'
         Site            = '$site'
-        Name            = '$apppath'
-        PhysicalPath    = '$($webapp.PhysicalPath)'
-        ApplicationPool = '$($webapp.applicationPool)'
+        PhysicalPath    = '$physicalpath'
+        ApplicationPool = '$pool'
     } |% {New-WebApplication @_}}
     else
-    {Write-Verbose 'Web application $path found'}
+    {Write-Verbose 'Web application $iispath found'}
 "@
-        Select-Xml "/configuration/location[@path='$site/$apppath']/system.webServer" (Get-WebConfigFile) |
+        Select-Xml "/configuration/location[@path='$site/$name']/system.webServer" (Get-WebConfigFile) |
             % {$_.Node.SelectNodes('*')} -pv cfg |
             % {
-                if($_.LocalName -ne 'security') {"The $($_.LocalName) may be customized for $path"}
+                if($_.LocalName -ne 'security') {"The $($_.LocalName) may be customized for $iispath"}
                 else
                 {
                     if($cfg.access){[psobject]@{
@@ -322,8 +435,8 @@ function $funcname
                     }}
                     $cfg.SelectNodes('authentication/*') |
                         % {
-                            if($_.userName){"Set $path username to $($_.userName)"}
-                            if($_.InnerXml){"Configure $path $($_.LocalName) for $($_.InnerXml -replace '(?m)^\s+|[\r\n]+','')"}
+                            if($_.userName){"Set $iispath username to $($_.userName)"}
+                            if($_.InnerXml){"Configure $iispath $($_.LocalName) for $($_.InnerXml -replace '(?m)^\s+|[\r\n]+','')"}
                             [pscustomobject]@{
                                 Filter = "system.webServer/security/authentication/$($_.LocalName)"
                                 Name   = 'enabled'
@@ -340,13 +453,14 @@ function $funcname
                 else
                 {@"
     try{Set-WebConfigurationProperty $cfgcontext -Filter '$($_.Filter)' -Name '$($_.Name)' -Value '$($_.Value)'}
-    catch{Write-Todo "Set $path $($_.Filter) $($_.Name) to '$($_.Value)'"}
+    catch{Write-Todo "Set $iispath $($_.Filter) $($_.Name) to '$($_.Value)'"}
 "@}}
         @"
 }
 "@
     }
     @"
+
 function Import-WebApplications
 {
     $($functions -join "`r`n    ")
@@ -361,15 +475,23 @@ function Export-Footer
     @"
 
 Ping-GlobalModules
+Import-Certificates
 Import-WebAppPools
 Import-Websites
 Import-WebApplications
 "@
 }
 
-Export-Header
-Export-GlobalModulesCheck
-Export-WebAppPools
-Export-Websites
-Export-WebApplications
-Export-Footer
+function Export-WebConfiguration
+{
+    if(!(Test-Administrator)) {throw 'This script must be run as administrator.'}
+    Export-Header
+    Export-Certificates
+    Export-GlobalModulesCheck
+    Export-WebAppPools
+    Export-Websites
+    Export-WebApplications
+    Export-Footer
+}
+
+Export-WebConfiguration |Out-File $Path utf8
