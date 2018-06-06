5243f31ebe31a610cb23c08d5f2e2c527cd9fcba
Brian Lalonde <brianary@gmail.com>
2018-06-06 16:53:03 -0700

    Create shortcuts, read ASP event; add defaults.

---
+++ Get-ClassicAspEvents.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.Synopsis
+    Gets Classic ASP errors from the event log on the given server.
+
+.Parameter ComputerName
+    The name of the server on which the error occurred.
+
+.Parameter EntryType
+    Gets only events with the specified entry type. Valid values are Error, Information, and Warning. The default is all events.
+
+.Parameter After
+    Skip events older than this datetime.
+
+.Parameter Before
+    Skip events newer than this datetime.
+
+.Parameter Newest
+    The maximum number of the most recent events to return.
+#>
+
+#requires -version 3
+[CmdletBinding()] Param(
+[Parameter(Position=0)][Alias('CN','Server')][string[]]$ComputerName = $env:COMPUTERNAME,
+[ValidateSet('Information','Warning','Error')][string[]]$EntryType,
+[DateTime]$After,
+[DateTime]$Before,
+[int]$Newest
+)
+$EventQuery = @{
+    ComputerName = $ComputerName
+    LogName      = 'Application'
+    Source       = 'Active Server Pages'
+}
+if($After){$EventQuery.After=$After}
+if($Before){$EventQuery.Before=$Before}
+if($Newest){$EventQuery.Newest=$Newest}
+if($EntryType){$EventQuery.EntryType=$EntryType}
+Get-EventLog @EventQuery
---
+++ New-Shortcut.ps1
@@ -0,0 +1,87 @@
+﻿<#
+.Synopsis
+    Create a Windows shortcut.
+
+.Parameter Name
+    The filename of the shortcut, typically including a .lnk extension.
+
+.Parameter TargetPath
+    The path of the file the shortcut will point to.
+
+.Parameter Arguments
+    Any command-line parameters to pass to the TargetPath, if it is a program.
+
+.Parameter WorkingDirectory
+    The folder to run TargetPath in.
+
+.Parameter Description
+    Some descriptive text for the shortcut.
+
+.Parameter Hotkey
+    A Windows Explorer key combination to open the shortcut, usually starting with
+    "Ctrl + Alt +".
+
+.Parameter IconLocation
+    The path to a file with an icon to use, and an index, e.g.
+
+        %SystemRoot%\system32\SHELL32.dll,244
+
+.Parameter WindowStyle
+    The state the window should start in: Normal, Maximized, or Minimized.
+
+.Parameter RunAsAdministrator
+    Indicates the shortcut should invoke UAC and run as an admin.
+
+.Link
+    https://ss64.com/vb/shortcut.html
+
+.Example
+    New-Shortcut -Path "$Home\Desktop\Explorer.lnk" -TargetPath '%SystemRoot%\explorer.exe' -RunAsAdministrator
+
+    Creates an Explorer shortcut on the desktop that runs as admin.
+#>
+
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true)][Alias('Name')][string] $Path,
+[Parameter(Position=1,Mandatory=$true)][string] $TargetPath,
+[Parameter(Position=2)][string] $Arguments,
+[Parameter(Position=3)][Alias('StartIn')][string] $WorkingDirectory,
+[Alias('Comment')][string] $Description,
+[Alias('ShortcutKey')][string] $Hotkey,
+[string] $IconLocation,
+[Alias('Run')][ValidateSet('Normal','Maximized','Minimized')][string] $WindowStyle,
+[Alias('Admin')][switch] $RunAsAdministrator
+)
+
+$sh = New-Object -ComObject WScript.Shell
+$path = Split-Path $Name
+# the old WshShortcut COM component doesn't support Unicode
+$file = [Net.WebUtility]::UrlEncode((Split-Path $Name -Leaf))
+$fullname = Join-Path $path $file
+$lnk = $sh.CreateShortcut($fullname)
+$lnk.TargetPath = $TargetPath
+if($Arguments)        {$lnk.Arguments = $Arguments}
+if($WorkingDirectory) {$lnk.WorkingDirectory = $WorkingDirectory}
+if($Description)      {$lnk.Description = $Description}
+if($Hotkey)           {$lnk.Hotkey = $Hotkey}
+if($IconLocation)     {$lnk.IconLocation = $IconLocation}
+if($WindowStyle)
+{
+    $lnk.WindowStyle =
+        switch($WindowStyle)
+        {
+            Normal    {1}
+            Maximized {3}
+            Minimized {7}
+        }
+}
+$lnk.Save()
+$lnk = $null
+$sh = $null
+if($RunAsAdministrator)
+{ # klugdy hack
+    [byte[]]$lnkdata = Get-Content $fullname -Encoding Byte
+    $lnkdata[0x15] = 0x20
+    $lnkdata |Set-Content $fullname -Encoding Byte
+}
+Rename-Item $fullname ([Net.WebUtility]::UrlDecode($file))
