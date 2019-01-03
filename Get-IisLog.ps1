ec82c704c4d314467a3fae54f6a9bd6aa6c9dbbd
Brian Lalonde <brianary@gmail.com>
2019-01-03 11:45:12 -0800

    Add HTTP Method searching

--- Get-IisLog.ps1
+++ Get-IisLog.ps1
@@ -23,6 +23,9 @@
 .Parameter Status
     The HTTP (major) status to restrict the search to.
 
+.Parameter Method
+    The HTTP method (GET or POST, &c) to restrict the search to.
+
 .Parameter UriPathLike
     A "like" pattern to match against the requested URI stem/path.
 
@@ -63,20 +66,20 @@
 
 .Example
     Get-IisLog.ps1 -LogDirectory \\Server\c$\inetpub\logs\LogFiles\W3SVC1 -After 2014-03-30 -UriPathLike '/WebApp/%' |select -First 1
- 
+
     Time      : 2014-03-31 15:56:45
     Server    : 192.168.1.99:80
     Filename  : ex140331.log
     Line      : 121555
     IpAddr    : 192.168.1.199
-    Username  : 
-    UserAgent : Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 
-                2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; 
+    Username  :
+    UserAgent : Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR
+                2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E;
                 InfoPath.3)
     Method    : GET
     UriPath   : /WebApp/
-    Query     : 
-    Referrer  : 
+    Query     :
+    Referrer  :
     Status    : 401.2
 #>
 
@@ -89,15 +92,22 @@
 [Alias("ClientIP")][string[]] $IpAddr,
 [string[]] $Username,
 [int[]] $Status,
+[Microsoft.PowerShell.Commands.WebRequestMethod[]] $Method,
 [string] $UriPathLike,
 [string] $QueryLike,
 [Alias("RefererLike")][string] $ReferrerLike
 )
 
-Use-Command.ps1 logparser "${env:ProgramFiles(x86)}\Log Parser 2.2\LogParser.exe" -msi http://download.microsoft.com/download/f/f/1/ff1819f9-f702-48a5-bbc7-c9656bc74de8/LogParser.msi
+Use-Command.ps1 logparser "${env:ProgramFiles(x86)}\Log Parser 2.2\LogParser.exe" `
+    -msi http://download.microsoft.com/download/f/f/1/ff1819f9-f702-48a5-bbc7-c9656bc74de8/LogParser.msi
+
+function Format-DateTimeLiteral([Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][datetime]$DateTime)
+{
+    return "to_timestamp('$(Get-Date $DateTime -Format 'yyyy-MM-dd HH:mm:ss')','yyyy-MM-dd HH:mm:ss')"
+}
 
 # get the log files
-$LogDirectory = 
+$LogDirectory =
     if ($ComputerName) { $ComputerName |% {"\\$_\LogFiles$"} }
     else { $LogDirectory |% FullName }
 $from = ' from ' +
@@ -109,11 +119,12 @@ if($from -eq ' from ') { $from += $($LogDirectory|% {"$_\*.log"}) -join ',' }
 
 # build the where clause
 $where = @()
-$where += " where to_localtime(to_timestamp(date,time)) between to_timestamp('$(Get-Date $After -Format 'yyyy-MM-dd HH:mm:ss')','yyyy-MM-dd HH:mm:ss')"
-$where += "to_timestamp('$(Get-Date $Before -Format 'yyyy-MM-dd HH:mm:ss')','yyyy-MM-dd HH:mm:ss')"
+$where += " where to_localtime(to_timestamp(date,time)) between $(Format-DateTimeLiteral $After)"
+$where += Format-DateTimeLiteral $Before
 if($IpAddr) { $where += "c-ip in ('$(($IpAddr |% {$_ -replace "'","''"}) -join '')')" }
 if($Username) { $where += "cs-username in ('$(($Username |% {$_ -replace "'","''"}) -join '')')" }
 if($Status) { $where += "sc-status in ($($Status -join ';'))" }
+if($Method -and $Method.Length) { $where += "cs-method in ('$(($Method |% {"$_".ToUpperInvariant()}) -join "';'")')" }
 if($UriPathLike) { $where += "cs-uri-stem like '$($UriPathLike -replace "'","''")'" }
 if($QueryLike) { $where += "cs-uri-query like '$($QueryLike -replace "'","''")'" }
 if($ReferrerLike) { $where += "cs(Referer) like '$($ReferrerLike -replace "'","''")'" }
@@ -123,13 +134,13 @@ $where = $where -join "`n   and "
 $sql = @"
 select to_localtime(to_timestamp(date,time)) as Time,
        strcat(s-ip,strcat(':',to_string(s-port))) as Server,
-       extract_filename(LogFilename) as Filename, 
-       LogRow as Line, 
-       coalesce(c-ip,'') as IpAddress, 
+       extract_filename(LogFilename) as Filename,
+       LogRow as Line,
+       coalesce(c-ip,'') as IpAddress,
        cs-username as Username,
-       coalesce(replace_chr(cs(User-Agent),'+',' '),'') as UserAgent, 
+       coalesce(replace_chr(cs(User-Agent),'+',' '),'') as UserAgent,
        cs-method as Method,
-       coalesce(cs-uri-stem,'') as UriPath, 
+       coalesce(cs-uri-stem,'') as UriPath,
        coalesce(cs-uri-query,'') as Query,
        coalesce(cs(Referer),'') as Referrer,
        strcat(strcat(to_string(sc-status),'.'),to_string(sc-substatus)) as Status
@@ -139,7 +150,8 @@ $where
 "@
 Write-Verbose $sql
 logparser $sql -i:IISW3C -o:TSV -headers:off -stats:off |
-    ConvertFrom-Csv -Delimiter "`t" -Header 'Time','Server','Filename','Line','IpAddress','Username','UserAgent','Method','UriPath','Query','Referrer','Status' |
+    ConvertFrom-Csv -Delimiter "`t" -Header 'Time','Server','Filename','Line','IpAddress',
+        'Username','UserAgent','Method','UriPath','Query','Referrer','Status' |
     % {
         [datetime]$_.Time = $_.Time
         [long]$_.Line = $_.Line
