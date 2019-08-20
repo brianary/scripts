c490a77cd6d831fd2e8b69af1951c33a0bd6c990
Brian Lalonde <brianary@gmail.com>
2019-08-19 21:00:55 -0700

    Add installed programs search

---
+++ Find-InstalledPrograms.ps1
@@ -0,0 +1,36 @@
+<#
+.Synopsis
+    Searches installed programs.
+
+.Parameter Name
+    The product name to search for. SQL-style "like" wildcards are supported.
+
+.Link
+    Get-WmiObject
+    https://serverfault.com/questions/693264/with-powershell-get-exactly-the-same-application-list-as-in-add-remove-programms
+
+.Example
+    Find-InstalledPrograms.ps1 %powershell%
+
+    IdentifyingNumber : {65276649-728D-4AB9-AAEC-6EFF860B11EC}
+    Name              : PowerShell 6-x64
+    Vendor            : Microsoft Corporation
+    Version           : 6.1.2.0
+    Caption           : PowerShell 6-x64
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param(
+[string] $Name
+)
+
+try
+{
+    Get-WmiObject Win32Reg_AddRemovePrograms -Filter "DisplayName like '$($Name -replace "'","''")'" -EA Stop
+    Get-WmiObject Win32Reg_AddRemovePrograms64 -Filter "DisplayName like '$($Name -replace "'","''")'" -EA Stop
+}
+catch
+{
+    Write-Verbose "$_, trying Win32_Product"
+    Get-WmiObject Win32_Product -Filter "Name like '$($Name -replace "'","''")'"
+}
