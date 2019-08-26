21a752f1a53fea978f8f9154747fe37c391692bb
Brian Lalonde <brianl@stcu.org>
2019-08-26 11:45:44 -0700

    Add password generation

---
+++ New-Password.ps1
@@ -0,0 +1,27 @@
+<#
+.Summary
+    Generate a new password.
+
+.Parameter Length
+    The length of the password in characters.
+
+.Link
+    Invoke-RestMethod
+    https://duckduckgo.com/api
+
+.Example
+    New-Password.ps1 64
+
+    -pTs[_?B0S6uqqBquWfB%f*FWPO)X6AEt|>}(V&|%%A-n^OSw!Z9#G/3s=LL;(Uq
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true)][int]$Length
+)
+try
+{
+    $a = Invoke-RestMethod "https://api.duckduckgo.com/?q=pwgen+strong+$Length&format=json"
+    [Net.HttpUtility]::HtmlDecode($a.Answer) -replace ' \(random password\)\z',''
+}
+catch {[Web.Security.Membership]::GeneratePassword($Length,3)}
