16546e57225fdb6925de6cf6034a7b4080ad8ea4
Brian Lalonde <brianl@stcu.org>
2019-08-26 11:59:28 -0700

    Add secure string support

--- New-Password.ps1
+++ New-Password.ps1
@@ -5,6 +5,9 @@
 .Parameter Length
     The length of the password in characters.
 
+.Parameter AsSecureString
+    Converts the password to a secure string.
+
 .Link
     Invoke-RestMethod
     https://duckduckgo.com/api
@@ -17,11 +20,15 @@
 
 #Requires -Version 3
 [CmdletBinding()] Param(
-[Parameter(Position=0,Mandatory=$true)][int]$Length
+[Parameter(Position=0,Mandatory=$true)][int]$Length,
+[Alias('SecureString')][switch]$AsSecureString
 )
-try
-{
-    $a = Invoke-RestMethod "https://api.duckduckgo.com/?q=pwgen+strong+$Length&format=json"
-    [Net.HttpUtility]::HtmlDecode($a.Answer) -replace ' \(random password\)\z',''
-}
-catch {[Web.Security.Membership]::GeneratePassword($Length,3)}
+$pwd =
+    try
+    {
+        $a = Invoke-RestMethod "https://api.duckduckgo.com/?q=pwgen+strong+$Length&format=json"
+        [Net.HttpUtility]::HtmlDecode($a.Answer) -replace ' \(random password\)\z',''
+    }
+    catch {[Web.Security.Membership]::GeneratePassword($Length,3)}
+if($AsSecureString) {ConvertTo-SecureString $pwd -AsPlainText -Force}
+else {$pwd}
