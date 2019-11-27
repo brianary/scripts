38535097f5dbcb9f945101d0bf4822ba7c0c423e
Brian Lalonde <brianary@gmail.com>
2019-11-27 13:49:39 -0800

    Add module updater

---
+++ Update-Modules.ps1
@@ -0,0 +1,19 @@
+<#
+.Synopsis
+    Cleans up old modules.
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param(
+[switch] $KeepOldModules
+)
+
+Update-Module -Force
+foreach($module in Get-Module -List |group Name |where Count -gt 1)
+{
+	$newestversion = $module.Group.Version |sort -Descending |select -First 1
+	foreach($oldmodule in $module.Group |where Version -lt $newestversion)
+	{
+		Uninstall-Module $oldmodule.Name -RequiredVersion $oldmodule.Version
+	}
+}
