fdc75d0e170d59a52ef40e99239ae7cc09309aa0
Brian Lalonde <brianary@gmail.com>
2020-04-02 11:57:00 -0700

    Add DNS addresses param

--- Get-Dns.ps1
+++ Get-Dns.ps1
@@ -5,6 +5,10 @@
 .Parameter HostName
     A host name or address to look up.
 
+.Parameter OnlyAddresses
+    Indicates that only the string versions of addresses belonging to the specified family should be returned.
+    "Unknown" returns all addresses.
+
 .Link
     https://msdn.microsoft.com/library/ms143998.aspx
 
@@ -18,7 +22,17 @@
 
 [CmdletBinding()] Param(
 [Parameter(Position=0,Mandatory=$true,ValueFromRemainingArguments=$true,ValueFromPipeline=$true)]
-[Alias('Address','HostAddress','Name')][string[]]$HostName
+[Alias('Address','HostAddress','Name')][string[]] $HostName,
+[Net.Sockets.AddressFamily] $OnlyAddresses
 )
 
-Process { $HostName |% {[net.dns]::GetHostEntry($_)} }
+Process
+{
+    foreach ($h in $HostName)
+    {
+        $entry = [Net.Dns]::GetHostEntry($h)
+        if(!$PSBoundParameters.ContainsKey('OnlyAddresses')) {$entry}
+        elseif($OnlyAddresses -eq [Net.Sockets.AddressFamily]::Unspecified) {$entry.AddressList |foreach {$_.IPAddressToString}}
+        else {$entry.AddressList |where AddressFamily -eq $OnlyAddresses |foreach {$_.IPAddressToString}}
+    }
+}
