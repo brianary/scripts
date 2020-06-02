f1e6e0b9c2047c3616bea62fb10c1ad7f6f4ec08
Brian Lalonde <brianl@stcu.org>
2020-06-02 09:41:25 -0700

    Add support for separating XML attributes

--- Format-Xml.ps1
+++ Format-Xml.ps1
@@ -1,87 +1,107 @@
 ﻿<#
 .Synopsis
-    Pretty-print XML.
+	Pretty-print XML.
 
 .Parameter Xml
-    The XML string or document to format.
+	The XML string or document to format.
 
 .Parameter IndentChar
-    A whitespace indent character to use, space by default.
+	A whitespace indent character to use, space by default.
 
 .Parameter Indentation
-    The number of IndentChars to use per level of indent, 2 by default.
-    Set to zero for no indentation.
+	The number of IndentChars to use per level of indent, 2 by default.
+	Set to zero for no indentation.
+
+.Parameter NewLineOnAttributes
+	Indicates attributes should be written on a new line.
 
 .Inputs
-    System.Xml.XmlDocument to serialize.
+	System.Xml.XmlDocument to serialize.
 
 .Outputs
-    System.String containing the serialized XML document with desired indents.
+	System.String containing the serialized XML document with desired indents.
 
 .Example
-    ls README.md |ConvertTo-Xml |Format-Xml.ps1
+	Get-PSProvider alias |ConvertTo-Xml |Format-Xml.ps1
 
-    <Objects>
-      <Object Type="System.IO.FileInfo">
-        <Property Name="PSPath" Type="System.String">Microsoft.PowerShell.Core\FileSystem::C:\Users\brian\OneDrive\Documents\GitHub\scripts\README.md</Property>
-        <Property Name="PSParentPath" Type="System.String">Microsoft.PowerShell.Core\FileSystem::C:\Users\brian\OneDrive\Documents\GitHub\scripts</Property>
-        <Property Name="PSChildName" Type="System.String">README.md</Property>
-        <Property Name="PSDrive" Type="System.Management.Automation.PSDriveInfo">C</Property>
-        <Property Name="PSProvider" Type="System.Management.Automation.ProviderInfo">Microsoft.PowerShell.Core\FileSystem</Property>
-        <Property Name="PSIsContainer" Type="System.Boolean">False</Property>
-        <Property Name="Mode" Type="System.String">-a----</Property>
-        <Property Name="VersionInfo" Type="System.Diagnostics.FileVersionInfo">File:             C:\Users\brian\OneDrive\Documents\GitHub\scripts\README.md
-      InternalName:     
-      OriginalFilename: 
-      FileVersion:      
-      FileDescription:  
-      Product:          
-      ProductVersion:   
-      Debug:            False
-      Patched:          False
-      PreRelease:       False
-      PrivateBuild:     False
-      SpecialBuild:     False
-      Language:         
-      </Property>
-        <Property Name="BaseName" Type="System.String">README</Property>
-        <Property Name="Target" Type="System.Collections.Generic.List`1[System.String]" />
-        <Property Name="LinkType" Type="System.String" />
-        <Property Name="Name" Type="System.String">README.md</Property>
-        <Property Name="Length" Type="System.Int64">10036</Property>
-        <Property Name="DirectoryName" Type="System.String">C:\Users\brian\OneDrive\Documents\GitHub\scripts</Property>
-        <Property Name="Directory" Type="System.IO.DirectoryInfo">C:\Users\brian\OneDrive\Documents\GitHub\scripts</Property>
-        <Property Name="IsReadOnly" Type="System.Boolean">False</Property>
-        <Property Name="Exists" Type="System.Boolean">True</Property>
-        <Property Name="FullName" Type="System.String">C:\Users\brian\OneDrive\Documents\GitHub\scripts\README.md</Property>
-        <Property Name="Extension" Type="System.String">.md</Property>
-        <Property Name="CreationTime" Type="System.DateTime">2017-02-03 22:26:47</Property>
-        <Property Name="CreationTimeUtc" Type="System.DateTime">2017-02-04 06:26:47</Property>
-        <Property Name="LastAccessTime" Type="System.DateTime">2017-07-30 09:51:27</Property>
-        <Property Name="LastAccessTimeUtc" Type="System.DateTime">2017-07-30 16:51:27</Property>
-        <Property Name="LastWriteTime" Type="System.DateTime">2017-07-29 23:35:01</Property>
-        <Property Name="LastWriteTimeUtc" Type="System.DateTime">2017-07-30 06:35:01</Property>
-        <Property Name="Attributes" Type="System.IO.FileAttributes">Archive</Property>
-      </Object>
-    </Objects>
+	<Objects>
+	  <Object Type="System.Management.Automation.ProviderInfo">
+	    <Property Name="ImplementingType" Type="System.RuntimeType">Microsoft.PowerShell.Commands.AliasProvider</Property>
+	    <Property Name="HelpFile" Type="System.String">System.Management.Automation.dll-Help.xml</Property>
+	    <Property Name="Name" Type="System.String">Alias</Property>
+	    <Property Name="PSSnapIn" Type="System.Management.Automation.PSSnapInInfo">Microsoft.PowerShell.Core</Property>
+	    <Property Name="ModuleName" Type="System.String">Microsoft.PowerShell.Core</Property>
+	    <Property Name="Module" Type="System.Management.Automation.PSModuleInfo" />
+	    <Property Name="Description" Type="System.String"></Property>
+	    <Property Name="Capabilities" Type="System.Management.Automation.Provider.ProviderCapabilities">ShouldProcess</Property>
+	    <Property Name="Home" Type="System.String"></Property>
+	    <Property Name="Drives" Type="System.Collections.ObjectModel.Collection`1[System.Management.Automation.PSDriveInfo]">
+	      <Property Type="System.Management.Automation.PSDriveInfo">Alias</Property>
+	    </Property>
+	  </Object>
+	</Objects>
+
+.Example
+	Get-PSProvider alias |ConvertTo-Xml |Format-Xml.ps1 -NewLineOnAttributes
+
+	<Objects>
+	  <Object
+	    Type="System.Management.Automation.ProviderInfo">
+	    <Property
+	      Name="ImplementingType"
+	      Type="System.RuntimeType">Microsoft.PowerShell.Commands.AliasProvider</Property>
+	    <Property
+	      Name="HelpFile"
+	      Type="System.String">System.Management.Automation.dll-Help.xml</Property>
+	    <Property
+	      Name="Name"
+	      Type="System.String">Alias</Property>
+	    <Property
+	      Name="PSSnapIn"
+	      Type="System.Management.Automation.PSSnapInInfo">Microsoft.PowerShell.Core</Property>
+	    <Property
+	      Name="ModuleName"
+	      Type="System.String">Microsoft.PowerShell.Core</Property>
+	    <Property
+	      Name="Module"
+	      Type="System.Management.Automation.PSModuleInfo" />
+	    <Property
+	      Name="Description"
+	      Type="System.String"></Property>
+	    <Property
+	      Name="Capabilities"
+	      Type="System.Management.Automation.Provider.ProviderCapabilities">ShouldProcess</Property>
+	    <Property
+	      Name="Home"
+	      Type="System.String"></Property>
+	    <Property
+	      Name="Drives"
+	      Type="System.Collections.ObjectModel.Collection`1[System.Management.Automation.PSDriveInfo]">
+	      <Property
+	        Type="System.Management.Automation.PSDriveInfo">Alias</Property>
+	    </Property>
+	  </Object>
+	</Objects>
 #>
 
 [CmdletBinding()][OutputType([string])] Param(
-[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][xml]$Xml,
-[ValidatePattern('\s')][char]$IndentChar = ' ',
-[int]$Indentation = 2
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][xml] $Xml,
+[ValidatePattern('\s')][char] $IndentChar = ' ',
+[int] $Indentation = 2,
+[Alias('SplitAttributes','AttributesSeparated')][switch] $NewLineOnAttributes
 )
 Process
 {
-    [Xml.XmlWriterSettings] $cfg = New-Object Xml.XmlWriterSettings -Property @{
-        Indent             = !!$Indentation
-        IndentChars        = "$IndentChar" * $Indentation
-        OmitXmlDeclaration = $true
-    }
-    $sw = New-Object IO.StringWriter
-    [Xml.XmlWriter] $xw = [Xml.XmlWriter]::Create($sw, $cfg)
-    $Xml.WriteTo($xw)
-    $xw.Dispose()
-    $sw.ToString()
-    $sw.Dispose()
+	[Xml.XmlWriterSettings] $cfg = New-Object Xml.XmlWriterSettings -Property @{
+		Indent              = !!$Indentation
+		IndentChars         = "$IndentChar" * $Indentation
+		OmitXmlDeclaration  = $true
+		NewLineOnAttributes = $NewLineOnAttributes
+	}
+	$sw = New-Object IO.StringWriter
+	[Xml.XmlWriter] $xw = [Xml.XmlWriter]::Create($sw, $cfg)
+	$Xml.WriteTo($xw)
+	$xw.Dispose()
+	$sw.ToString()
+	$sw.Dispose()
 }
