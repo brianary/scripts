b797275ae43449c938e0ae0bfdb26eaf3855183f
Brian Lalonde <brianary@gmail.com>
2020-06-17 21:43:01 -0700

    Add input info

--- Add-Utf8Signature.ps1
+++ Add-Utf8Signature.ps1
@@ -1,23 +1,26 @@
 <#
 .Synopsis
-    Adds the utf-8 signature (BOM) to a file.
+	Adds the utf-8 signature (BOM) to a file.
 
 .Parameter Path
-    The file to add the utf-8 signature to.
+	The file to add the utf-8 signature to.
+
+.Inputs
+	System.String containing the path to the file to be updated.
 
 .Link
-    https://msdn.microsoft.com/library/s064f8w2aspx
+	https://msdn.microsoft.com/library/s064f8w2aspx
 
 .Link
-    https://msdn.microsoft.com/library/ms143376aspx
+	https://msdn.microsoft.com/library/ms143376aspx
 
 .Link
-    Get-Content
+	Get-Content
 
 .Example
-    Add-Utf8Signature.ps1 README.md
+	Add-Utf8Signature.ps1 README.md
 
-    Adds the EF BB BF at the beginning of the file, warns if it isn't found.
+	Adds the EF BB BF at the beginning of the file, warns if it isn't found.
 #>
 
 #Requires -Version 4
@@ -26,8 +29,8 @@
 )
 Process
 {
-    Write-Verbose "Adding utf-8 BOM/SIG from $Path"
-    if(Test-FileTypeMagicNumber.ps1 utf8 $Path){Write-Warning "A utf-8 signature was already found in $Path"; return}
-    # this will have to do until PS6 http://brianary.github.io/powershell-encoding.html
-    [IO.File]::WriteAllText((Resolve-Path $Path),(Get-Content $Path -Raw),(New-Object System.Text.UTF8Encoding $true))
+	Write-Verbose "Adding utf-8 BOM/SIG from $Path"
+	if(Test-FileTypeMagicNumber.ps1 utf8 $Path){Write-Warning "A utf-8 signature was already found in $Path"; return}
+	# this will have to do until PS6 http://brianary.github.io/powershell-encoding.html
+	[IO.File]::WriteAllText((Resolve-Path $Path),(Get-Content $Path -Raw),(New-Object System.Text.UTF8Encoding $true))
 }
