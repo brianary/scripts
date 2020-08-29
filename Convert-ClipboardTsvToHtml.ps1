16224303cce82d4b0ab2b2678d09bf9470bbaca2
Brian Lalonde <brianary@gmail.com>
2020-08-28 20:37:58 -0700

    Add clipboard TSV to HTML table conversion

---
+++ Convert-ClipboardTsvToHtml.ps1
@@ -0,0 +1,15 @@
+<#
+.Synopsis
+	Parses TSV clipboard data into HTML table data which is copied back to the clipboard.
+
+.Example
+	Convert-ClipboardTsvToHtml.ps1
+
+	TSV clipboard data may now be pasted into an email or document as a table.
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param()
+ConvertFrom-ClipboardTsv.ps1 |
+	ConvertTo-Html -Fragment |
+	Set-Clipboard -AsHtml
