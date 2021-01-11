366c6f62da5a632178c8fbbe01e5b313b9df0c3d
Brian Lalonde <brianl@stcu.org>
2021-01-11 08:24:46 -0800

    Annotate workarounds; fix more padding

--- Format-Date.ps1
+++ Format-Date.ps1
@@ -36,17 +36,17 @@ Process
 	{
 		FrenchRepublicanDateTime {"$(Get-FrenchRepublicanDate.ps1 $Date)"}
 		Iso8601 {Get-Date $Date -f "yyyy'-'MM'-'dd'T'HH':'mm':'sszzzz"}
-		Iso8601Date {Get-Date $Date -uf %F}
+		Iso8601Date {Get-Date $Date -uf %Y-%m-%d} # PS5 doesn't support %F
 		Iso8601OrdinalDate {Get-Date $Date -uf "%Y-$('{0:000}' -f $Date.DayOfYear)"}
 		Iso8601Week
 		{
-			$V = '{0:00}' -f [int](Get-Date $Date -uf %V)
+			$V = '{0:00}' -f [int](Get-Date $Date -uf %V) # PS5 doesn't zero-pad %V
 			if(53 -eq (Get-Date $Date -uf %V) -and $Date.Month -eq 1) {"$($Date.Year-1)-W$V"}
 			else {Get-Date $Date -uf %Y-W$V}
 		}
 		Iso8601WeekDate
 		{
-			$w = [int]$Date.DayOfWeek; if($w -eq 0) {$w = 7}
+			$w = [int]$Date.DayOfWeek; if($w -eq 0) {$w = 7} # PS5-7.1 formats Sundays as zero
 			$V = '{0:00}' -f [int](Get-Date $Date -uf %V)
 			if(53 -eq (Get-Date $Date -uf %V) -and $Date.Month -eq 1) {"$($Date.Year-1)-W$V-$w"}
 			else {Get-Date $Date -uf %Y-W$V-$w}
@@ -54,7 +54,7 @@ Process
 		Iso8601Z {"$(Get-Date $Date.ToUniversalTime() -f s)Z"}
 		LocalLongDate {Get-Date $Date -f D}
 		LocalLongDateTime {Get-Date $Date -f F}
-		Rfc1123 {Get-Date $Date -uf '%a, %e %b %Y %T %Z'}
-		Rfc1123Gmt {Get-Date $Date.ToUniversalTime() -f R}
+		Rfc1123 {Get-Date $Date -uf '%a, %e %b %Y %T %Z'} # CLR R format zero-pads date instead of space-padding like %e
+		Rfc1123Gmt {Get-Date $Date.ToUniversalTime() -uf '%a, %e %b %Y %T %Z'}
 	}
 }
