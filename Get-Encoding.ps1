27498d0232eaadfc5b496e89e99cef97b6416cb2
Brian Lalonde <brianary@gmail.com>
2021-05-29 16:17:03 -0700

    ✨ Add script to detect line endings

--- Get-Encoding.ps1
+++ Get-Encoding.ps1
@@ -23,23 +23,26 @@
 [CmdletBinding()][OutputType([Text.Encoding])] Param(
 [Parameter(Position=0,Mandatory=$true,ValueFromPipelineByPropertyName=$true)][Alias('FullName')][string] $Path
 )
-if($PSVersionTable.PSEdition -eq 'Core')
+Process
 {
-	if(Test-FileTypeMagicNumber.ps1 utf8 $Path) {[Text.Encoding]::UTF8}
-	elseif(Test-FileTypeMagicNumber.ps1 utf16 $Path) {[Text.Encoding]::Unicode}
-	elseif(Test-FileTypeMagicNumber.ps1 utf16be $Path) {[Text.Encoding]::BigEndianUnicode}
-	elseif(Test-FileTypeMagicNumber.ps1 utf32 $Path) {[Text.Encoding]::UTF32}
-	elseif(Test-FileTypeMagicNumber.ps1 utf32be $Path) {[Text.Encoding]::GetEncoding('UTF-32BE')}
-	elseif(Test-FileTypeMagicNumber.ps1 text $Path) {[Text.Encoding]::ASCII}
-	else {[Text.Encoding]::Default}
-}
-else
-{
-	if(Test-FileTypeMagicNumber.ps1 utf8 $Path) {'utf8'}
-	elseif(Test-FileTypeMagicNumber.ps1 utf16 $Path) {'unicode'}
-	elseif(Test-FileTypeMagicNumber.ps1 utf16be $Path) {'bigendianunicode'}
-	elseif(Test-FileTypeMagicNumber.ps1 utf32 $Path) {'utf32'}
-	elseif(Test-FileTypeMagicNumber.ps1 utf32be $Path) {'utf32be'} # not supported
-	elseif(Test-FileTypeMagicNumber.ps1 text $Path) {'ascii'}
-	else {'default'}
+	if($PSVersionTable.PSEdition -eq 'Core')
+	{
+		if(Test-FileTypeMagicNumber.ps1 utf8 $Path) {[Text.Encoding]::UTF8}
+		elseif(Test-FileTypeMagicNumber.ps1 utf16 $Path) {[Text.Encoding]::Unicode}
+		elseif(Test-FileTypeMagicNumber.ps1 utf16be $Path) {[Text.Encoding]::BigEndianUnicode}
+		elseif(Test-FileTypeMagicNumber.ps1 utf32 $Path) {[Text.Encoding]::UTF32}
+		elseif(Test-FileTypeMagicNumber.ps1 utf32be $Path) {[Text.Encoding]::GetEncoding('UTF-32BE')}
+		elseif(Test-FileTypeMagicNumber.ps1 text $Path) {[Text.Encoding]::ASCII}
+		else {[Text.Encoding]::Default}
+	}
+	else
+	{
+		if(Test-FileTypeMagicNumber.ps1 utf8 $Path) {'utf8'}
+		elseif(Test-FileTypeMagicNumber.ps1 utf16 $Path) {'unicode'}
+		elseif(Test-FileTypeMagicNumber.ps1 utf16be $Path) {'bigendianunicode'}
+		elseif(Test-FileTypeMagicNumber.ps1 utf32 $Path) {'utf32'}
+		elseif(Test-FileTypeMagicNumber.ps1 utf32be $Path) {'utf32be'} # not supported
+		elseif(Test-FileTypeMagicNumber.ps1 text $Path) {'ascii'}
+		else {'default'}
+	}
 }
