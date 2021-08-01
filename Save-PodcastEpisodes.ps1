e119fdacd57251b135d8421585c25c54e18b9f25
Brian Lalonde <brianary@gmail.com>
2021-08-01 13:36:18 -0700

    Add podcast saving script

---
+++ Save-PodcastEpisodes.ps1
@@ -0,0 +1,82 @@
+<#
+.Synopsis
+	Saves enclosures from a podcast feed.
+
+.Parameter Uri
+	The URL of the podcast feed.
+
+.Parameter After
+	Episodes before this date will be ignored.
+
+.Parameter Before
+	Episodes after this date will be ignored.
+
+.Parameter First
+	Includes only the given number of initial episodes, by publish date.
+
+.Parameter Last
+	Includes only the given number of most recent episodes, by publish date.
+
+.Parameter UseTitle
+	Use episode titles for filenames.
+
+.Parameter CreateFolder
+	Saves the episodes into a folder with the podcast name.
+
+.Link
+	Save-WebRequest.ps1
+
+.Example
+	Save-PodcastEpisodes.ps1 https://www.youlooknicetoday.com/rss -UseTitle
+
+	Saves podcast episodes to the current directory.
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][Alias('Url')][uri] $Uri,
+[datetime] $After,
+[datetime] $Before,
+[int] $First,
+[int] $Last,
+[switch] $UseTitle,
+[switch] $CreateFolder
+)
+Begin
+{
+	[regex] $invalidchars = "(?:$(([io.path]::GetInvalidFileNameChars() |foreach {'\x{0:X2}' -f [int]$_}) -join '|'))+"
+}
+Process
+{
+	$channel = ([xml]((Invoke-WebRequest $Uri).Content)).rss.channel
+	$channel |Format-List |Out-String |Write-Verbose
+	$activity = "Downloading $($channel.title)"
+	[object[]] $episodes = Invoke-RestMethod $Uri
+	foreach($episode in $episodes) {$episode |Add-Member published ([datetime]$episode.pubDate)}
+	if($PSBoundParameters.ContainsKey('After')) {[object[]] $episodes = $episodes |where published -gt $After}
+	if($PSBoundParameters.ContainsKey('Before')) {[object[]] $episodes = $episodes |where published -lt $Before}
+	if($PSBoundParameters.ContainsKey('First')) {[object[]] $episodes = $episodes |sort published |select -First $First}
+	if($PSBoundParameters.ContainsKey('Last')) {[object[]] $episodes = $episodes |sort published |select -Last $Last}
+	if($CreateFolder) {New-Item ($channel.title -replace $invalidchars,'_') -ItemType Directory |Push-Location}
+	$i,$max = 0,($episodes.Count/100)
+	foreach($episode in $episodes)
+	{
+		$episode |Format-List |Out-String |Write-Verbose
+		$title = $episode.title |select -First 1
+		Write-Progress $activity $title -curr $episode.enclosure.url -percent ($i++/$max)
+		if($UseTitle)
+		{
+			$filename = if($episode.PSObject.Properties.Match('episode')) {$episode.episode + ' '} else {''}
+			$filename += $title -replace $invalidchars,'_'
+			$filename += [io.path]::GetExtension($episode.enclosure.url)
+			Invoke-WebRequest $episode.enclosure.url -OutFile $filename
+			(Get-Item $filename).CreationTime = $episode.published
+		}
+		else
+		{
+			Save-WebRequest.ps1 $episode.enclosure.url -CreationTime $episode.published
+		}
+	}
+	if($CreateFolder) {Pop-Location}
+	Write-Progress $activity -Completed
+}
--- Save-WebRequest.ps1
+++ Save-WebRequest.ps1
@@ -1,9 +1,15 @@
 <#
 .Synopsis
-    Downloads a given URL to a file, automatically determining the filename.
+	Downloads a given URL to a file, automatically determining the filename.
 
 .Parameter Uri
-    The URL to download.
+	The URL to download.
+
+.Parameter CreationTime
+	Sets the creation time on the file to the given value.
+
+.Parameter LastWriteTime
+	Sets the creation time on the file to the given value.
 
 .Parameter Open
 	When present, invokes the file after it is downloaded.
@@ -12,50 +18,54 @@
 	Object with System.Uri property named Uri.
 
 .Link
-    https://tools.ietf.org/html/rfc2183
+	https://tools.ietf.org/html/rfc2183
 
 .Link
-    http://test.greenbytes.de/tech/tc2231/
+	http://test.greenbytes.de/tech/tc2231/
 
 .Link
-    https://msdn.microsoft.com/library/system.net.mime.contentdisposition.filename.aspx
+	https://msdn.microsoft.com/library/system.net.mime.contentdisposition.filename.aspx
 
 .Link
-    https://msdn.microsoft.com/library/system.io.path.getinvalidfilenamechars.aspx
+	https://msdn.microsoft.com/library/system.io.path.getinvalidfilenamechars.aspx
 
 .Link
-    Invoke-WebRequest
+	Invoke-WebRequest
 
 .Link
-    Invoke-Item
+	Invoke-Item
 
 .Link
-    Move-Item
+	Move-Item
 
 .Example
-    Save-WebRequest.ps1 https://www.irs.gov/pub/irs-pdf/f1040.pdf -Open
+	Save-WebRequest.ps1 https://www.irs.gov/pub/irs-pdf/f1040.pdf -Open
 
-    Saves f1040.pdf (or else a filename specified in the Content-Disposition header) and opens it.
+	Saves f1040.pdf (or else a filename specified in the Content-Disposition header) and opens it.
 #>
 
 [CmdletBinding()][OutputType([void])] Param(
-[Parameter(Position=0,Mandatory=$true,ValueFromPipelineByPropertyName=$true)][uri]$Uri,
-[switch]$Open
+[Parameter(Position=0,Mandatory=$true,ValueFromPipelineByPropertyName=$true)][Alias('Url')][uri] $Uri,
+[datetime] $CreationTime,
+[datetime] $LastWriteTime,
+[switch] $Open
 )
 Begin
 {
-    function Get-ValidFileName([string]$filename)
-    {($filename -replace '(\A.*[\\/])?','').Split([IO.Path]::GetInvalidFileNameChars()) -join ''}
+	function Get-ValidFileName([string]$filename)
+	{($filename -replace '(\A.*[\\/])?','').Split([IO.Path]::GetInvalidFileNameChars()) -join ''}
 }
 Process
 {
-    $filename = Get-ValidFileName $Uri.Segments[-1]
-    $response = Invoke-WebRequest $Uri -OutFile $filename -PassThru
-    if(([Collections.IDictionary]$response.Headers).Contains('Content-Disposition'))
-    {
-        [Net.Mime.ContentDisposition]$disposition = $response.Headers['Content-Disposition']
-        $suggestedFilename = Get-ValidFileName $disposition.FileName
-        if($filename -cne $suggestedFilename) {Move-Item $filename $suggestedFilename; $filename = $suggestedFilename}
-    }
-    if($Open) {Invoke-Item $filename}
+	$filename = Get-ValidFileName $Uri.Segments[-1]
+	$response = Invoke-WebRequest $Uri -OutFile $filename -PassThru
+	if(([Collections.IDictionary]$response.Headers).Contains('Content-Disposition'))
+	{
+		[Net.Mime.ContentDisposition]$disposition = $response.Headers['Content-Disposition']
+		$suggestedFilename = Get-ValidFileName $disposition.FileName
+		if($filename -cne $suggestedFilename) {Move-Item $filename $suggestedFilename; $filename = $suggestedFilename}
+	}
+	if($PSBoundParameters.ContainsKey('CreationTime')) {(Get-Item $filename).CreationTime = $CreationTime}
+	if($PSBoundParameters.ContainsKey('LastWriteTime')) {(Get-Item $filename).LastWriteTime = $LastWriteTime}
+	if($Open) {Invoke-Item $filename}
 }
