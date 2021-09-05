10bd3b3084f4e354dda5ffd7a24313727af98bec
Brian Lalonde <brianary@gmail.com>
2021-09-05 16:20:18 -0700

    Improve visible string printing

--- Write-VisibleString.ps1
+++ Write-VisibleString.ps1
@@ -26,15 +26,31 @@
 [ConsoleColor] $AltColor = 'DarkYellow',
 [switch] $AsSymbols
 )
+$formatHex = {if([int]$_ -gt 0x100){'{0:X4}' -f [int]$_}else{'{0:X2}' -f [int]$_}}
 function Write-Strings([string[]] $String)
 {
-	if(![char]::IsControl($String[0][0])) {Write-Host $String[0] -NoNewline}
+	if(!$String) {return}
+	[char] $c = $String[0][0]
+	if(![char]::IsControl($c) -and ![char]::IsWhiteSpace($c) -and
+		![char]::IsLowSurrogate($c) -and ![char]::IsHighSurrogate($c))
+	{
+		Write-Host $String[0] -NoNewline
+	}
 	else
 	{
-		if($AsSymbols) {Write-Host ([string]($String[0].ToCharArray() |foreach {[char](0x2400+[int]$_)})) -NoNewline -ForegroundColor $AltColor}
-		else {Write-Host (' '+(($String[0].ToCharArray() |foreach {'{0:X2} ' -f [int]$_}) -join ' ')) -NoNewline -ForegroundColor $AltColor}
+		if($AsSymbols -and ([int]$c) -le 0x20)
+		{
+			$Local:OFS = ''
+			Write-Host ([string]($String[0].ToCharArray() |foreach {[char](0x2400+[int]$_)})) `
+				-NoNewline -ForegroundColor $AltColor
+		}
+		else
+		{
+			Write-Host " $($String[0].GetEnumerator() |foreach $formatHex) " `
+				-NoNewline -ForegroundColor $AltColor
+		}
 	}
 	if($String.Length -gt 1) {Write-Strings ($String[1..($String.Length-1)])}
 	else {Write-Host}
 }
-Write-Strings ([convert]::ToString($InputObject) -split '(\p{Cc}+)')
+Write-Strings ([convert]::ToString($InputObject) -split '((?:\p{C}+|\p{Z}+))')
