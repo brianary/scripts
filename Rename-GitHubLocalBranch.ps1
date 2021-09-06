2f60215aa4b361c35e0417cf793b6bcb2dcbdcb7
Brian Lalonde <brianary@gmail.com>
2021-09-05 20:05:01 -0700

    Support easy branch renaming

---
+++ Rename-GitHubLocalBranch.ps1
@@ -0,0 +1,52 @@
+<#
+.Synopsis
+	Rename a git repository branch.
+
+.Parameter OldName
+	The current branch name.
+
+.Parameter NewName
+	The new branch name.
+
+.Link
+	https://docs.github.com/en/github/administering-a-repository/managing-branches-in-your-repository/renaming-a-branch#updating-a-local-clone-after-a-branch-name-changes
+
+.Link
+	https://github.com/github/renaming
+
+.Link
+	Use-Command.ps1
+
+.Example
+	Rename-GitHubLocalBranch.ps1 master main
+
+	Rename the master branch to main.
+#>
+
+#Requires -Version 3
+[CmdletBinding(ConfirmImpact='High',SupportsShouldProcess=$true)] Param(
+[Parameter(Position=0,Mandatory=$true)][string] $NewName
+)
+
+if(!$PSCmdlet.ShouldContinue('Have you renamed the branch in the GitHub UI?','GitHub Status'))
+{
+	Write-Host 'Rename the branch via the GitHub UI before running this script.'
+	if((Get-Command gh -CommandType Application -ErrorAction SilentlyContinue)) {gh browse}
+	Start-Process 'https://docs.github.com/en/github/administering-a-repository/managing-branches-in-your-repository/renaming-a-branch#renaming-a-branch'
+	return
+}
+
+$oldName = git rev-parse --abbrev-ref HEAD
+if(!$PSCmdlet.ShouldProcess("$oldName branch","rename to $NewName")) {return}
+
+Use-Command.ps1 git "$env:ProgramFiles\Git\cmd\git.exe" -choco git
+Write-Verbose "git branch -m $oldName $NewName"
+git branch -m $oldName $NewName
+Write-Verbose "git fetch origin"
+git fetch origin
+Write-Verbose "git branch -u origin/$NewName $NewName"
+git branch -u "origin/$NewName" $NewName
+Write-Verbose 'git remote set-head origin -a'
+git remote set-head origin -a
+Write-Verbose 'git remote prune origin'
+git remote prune origin
