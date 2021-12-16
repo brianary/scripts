d7b2d8299492faf678c786cbcb694e0096a33574
Brian Lalonde <brianary@gmail.com>
2021-12-16 07:58:10 -0800

    Enhance host messages display

--- Update-Everything.ps1
+++ Update-Everything.ps1
@@ -9,30 +9,30 @@
 
 if((Get-Command Get-CimInstance -ErrorAction SilentlyContinue))
 {
-	Write-Host 'Updating Windows Store apps' -Fore DarkYellow
+	Write-Host "$(Get-Unicode.ps1 0x1F199) Updating Windows Store apps" -fore White -back DarkGray
 	Get-CimInstance MDM_EnterpriseModernAppManagement_AppManagement01 -Namespace root\cimv2\mdm\dmmap |
 		Invoke-CimMethod -MethodName UpdateScanMethod
 }
 if((Get-Command cup -ErrorAction SilentlyContinue))
 {
-	Write-Host 'Updating Chocolatey packages' -Fore DarkYellow
+	Write-Host "$(Get-Unicode.ps1 0x1F199) Updating Chocolatey packages" -fore White -back DarkGray
 	cup all -y
 }
 if((Get-Command npm -ErrorAction SilentlyContinue))
 {
-	Write-Host 'Updating npm packages' -Fore DarkYellow
+	Write-Host "$(Get-Unicode.ps1 0x1F199) Updating npm packages" -fore White -back DarkGray
 	npm update -g
 }
 if((Get-Command dotnet -ErrorAction SilentlyContinue))
 {
-	Write-Host 'Updating dotnet global tools' -Fore DarkYellow
+	Write-Host "$(Get-Unicode.ps1 0x1F199) Updating dotnet global tools" -fore White -back DarkGray
 	Get-DotNetGlobalTools.ps1 |foreach {dotnet tool update -g $_.Package}
 }
-Write-Host 'Updating PowerShell help' -Fore DarkYellow
+Write-Host "$(Get-Unicode.ps1 0x1F199) Updating PowerShell help" -fore White -back DarkGray
 Update-Help
 if((Get-Module PSWindowsUpdate -ListAvailable))
 {
-	Write-Host 'Updating Windows' -Fore DarkYellow
+	Write-Host "$(Get-Unicode.ps1 0x1F199) Updating Windows" -fore White -back DarkGray
 	Get-WindowsUpdate
 	Install-WindowsUpdate |Format-Table X,Result,KB,Size,Title
 }
