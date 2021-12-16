24c4b3afbb596d497042f055c7ec3dd8948124a3
Brian Lalonde <brianary@gmail.com>
2021-12-16 07:44:17 -0800

    Add Windows Store updates and host messages

--- Update-Everything.ps1
+++ Update-Everything.ps1
@@ -7,16 +7,32 @@
 #Requires -RunAsAdministrator
 [CmdletBinding()] Param()
 
-if((Get-Command cup -ErrorAction SilentlyContinue)) {Write-Verbose 'Chocolatey updates'; cup all -y}
-if((Get-Command npm -ErrorAction SilentlyContinue)) {Write-Verbose 'npm updates'; npm update -g}
+if((Get-Command Get-CimInstance -ErrorAction SilentlyContinue))
+{
+	Write-Host 'Updating Windows Store apps' -Fore DarkYellow
+	Get-CimInstance MDM_EnterpriseModernAppManagement_AppManagement01 -Namespace root\cimv2\mdm\dmmap |
+		Invoke-CimMethod -MethodName UpdateScanMethod
+}
+if((Get-Command cup -ErrorAction SilentlyContinue))
+{
+	Write-Host 'Updating Chocolatey packages' -Fore DarkYellow
+	cup all -y
+}
+if((Get-Command npm -ErrorAction SilentlyContinue))
+{
+	Write-Host 'Updating npm packages' -Fore DarkYellow
+	npm update -g
+}
 if((Get-Command dotnet -ErrorAction SilentlyContinue))
 {
-	Write-Verbose 'dotnet global tools updates'
+	Write-Host 'Updating dotnet global tools' -Fore DarkYellow
 	Get-DotNetGlobalTools.ps1 |foreach {dotnet tool update -g $_.Package}
 }
+Write-Host 'Updating PowerShell help' -Fore DarkYellow
+Update-Help
 if((Get-Module PSWindowsUpdate -ListAvailable))
 {
+	Write-Host 'Updating Windows' -Fore DarkYellow
 	Get-WindowsUpdate
 	Install-WindowsUpdate |Format-Table X,Result,KB,Size,Title
 }
-Update-Help
