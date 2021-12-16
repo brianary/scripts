bcfaac68dd02bde6e32f4fddeee89202d1a38a84
Brian Lalonde <brianary@gmail.com>
2021-12-15 21:02:27 -0800

    Add console history query

---
+++ Get-ConsoleHistory.ps1
@@ -0,0 +1,25 @@
+<#
+.Synopsis
+	Returns the DOSKey-style console command history (up arrow or F8).
+
+.Outputs
+	System.Management.Automation.PSObject with these properties:
+
+	* Id: The position of the command in the console history.
+	* CommandLine: The command entered in the history.
+
+.Example
+	Get-ConsoleHistory.ps1 |where CommandLine -like *readme*
+
+	Id CommandLine
+	-- -----------
+	30 gc .\README.md
+	56 gc .\README.md
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param()
+
+$id = 0
+Get-Content $env:AppData\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt |
+	foreach {[pscustomobject]@{Id=++$id;CommandLine=$_}}
