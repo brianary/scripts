ea37ec4bd4941d870b6f1ffedd57a6c23296f58d
Brian Lalonde <brianary@gmail.com>
2021-12-28 16:43:13 -0800

    Fix CIM conversion

--- ConvertFrom-CimInstance.ps1
+++ ConvertFrom-CimInstance.ps1
@@ -1,20 +1,20 @@
 <#
 .Synopsis
-    Convert a CimInstance object to a PSObject.
+	Convert a CimInstance object to a PSObject.
 
 .Parameter InputObject
-    The CimInstance object to convert to a PSObject.
+	The CimInstance object to convert to a PSObject.
 
 .Inputs
-    Microsoft.Management.Infrastructure.CimInstance to convert to a PSObject.
+	Microsoft.Management.Infrastructure.CimInstance to convert to a PSObject.
 
 .Outputs
-    PSObject converted from the CimInstance entered.
+	PSObject converted from the CimInstance entered.
 
 .Example
-    $tasks = Get-ScheduledTask |ConvertFrom-CimInstance.ps1
+	$tasks = Get-ScheduledTask |ConvertFrom-CimInstance.ps1
 
-    Gets the scheduled tasks as PSObjects that support tab completion and can be serialized and exported.
+	Gets the scheduled tasks as PSObjects that support tab completion and can be serialized and exported.
 #>
 
 #Requires -Version 3
@@ -24,16 +24,16 @@
 )
 Process
 {
-    $value = @{}
-    foreach($prop in $InputObject.CimInstanceProperties)
-    {
-        $value[$prop.Name] =
-            switch($prop.CimType)
-            {
-                Instance      {$prop.Value |ConvertFrom-CimInstance.ps1}
-                InstanceArray {@($prop.Value |ConvertFrom-CimInstance.ps1)}
-                default       {$prop.Value}
-            }
-    }
-    [pscustomobject]$value
+	$value = @{}
+	foreach($name in $InputObject.CimClass.CimClassProperties)
+	{
+		$value[$name] =
+			switch($InputObject.$name)
+			{
+				Instance      {$InputObject.$name |ConvertFrom-CimInstance.ps1}
+				InstanceArray {@($InputObject.$name |ConvertFrom-CimInstance.ps1)}
+				default       {$InputObject.$name}
+			}
+	}
+	[pscustomobject]$value
 }
