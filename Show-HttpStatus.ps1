ea6a530c30b8f20d2239967710b89533ee22ae54
Brian Lalonde <brianary@gmail.com>
2022-08-21 15:37:46 -0700

    Script to display HTTP status code info

---
+++ Show-HttpStatus.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+Displays the HTTP status code info.
+
+.EXAMPLE
+Show-HttpStatus.ps1 ServiceUnavailable -AsCat
+
+.EXAMPLE
+Show-HttpStatus.ps1 200 -AsCat
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param(
+# The HTTP status code to describe.
+[Parameter(Position=0,Mandatory=$true)][Net.HttpStatusCode] $Status,
+# Render the code as a cat.
+[switch] $AsCat
+)
+Begin
+{
+	if($AsCat -and !(Get-Command Out-ConsolePicture -ErrorAction SilentlyContinue))
+	{
+		Install-Module OutConsolePicture
+	}
+}
+Process
+{
+	"$([int]$Status) $Status"
+	"https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/$([int]$Status)"
+	if($AsCat)
+	{
+		$cat = "https://http.cat/$([int]$Status)"
+		Out-ConsolePicture -Url $cat
+		$cat
+	}
+}
