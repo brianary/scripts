bc3c6449a5df96a41d84d454fde73b4c2a9848b9
Brian Lalonde <brianary@gmail.com>
2022-10-28 22:02:52 -0700

    ✨ Support .NET (Core) by shelling out to .NET Framework
    Fix Use-NetMailConfig.ps1 doesn't work with PowerShell Core #10

--- Use-NetMailConfig.ps1
+++ Use-NetMailConfig.ps1
@@ -37,25 +37,36 @@ values from the ConfigurationManager.
 #>
 
 #Requires -Version 3
-#Requires -Modules @{ ModuleName='Microsoft.PowerShell.Utility'; MaximumVersion='3.1.0.0' }
 [CmdletBinding()][OutputType([void])] Param(
 # The scope to create the defaults in.
 [string] $Scope = 'Local',
 # Indicates the defaults should not be visible to child scopes.
 [switch] $Private
 )
-try{[void][Configuration.ConfigurationManager]}catch{Add-Type -as System.Configuration}
+
 $Scope = Add-ScopeLevel.ps1 $Scope
 $sv = if($Private) {@{Scope=$Scope;Option='Private'}} else {@{Scope=$Scope}}
-$smtp = [Configuration.ConfigurationManager]::GetSection('system.net/mailSettings/smtp')
-if($smtp.From) { Set-ParameterDefault.ps1 Send-MailMessage From $smtp.From @sv }
-if($smtp.DeliveryMethod -eq 'Network' -and $smtp.Network)
-{
-	if($smtp.Network.Host) {Set-ParameterDefault.ps1 Send-MailMessage SmtpServer $smtp.Network.Host @sv}
-	if($smtp.Network.EnableSsl) {Set-ParameterDefault.ps1 Send-MailMessage UseSsl $smtp.Network.EnableSsl @sv}
+
+if($PSVersionTable.PSEdition -eq 'Core')
+{ # from .NET (Core), run from .NET Framework, which has a machine.config to parse
+	$values = Invoke-WindowsPowerShell.ps1 { Use-NetMailConfig.ps1; $PSDefaultParameterValues }
+	$values.Keys |ForEach-Object {Set-ParameterDefault.ps1 Send-MailMessage `
+		($_ -replace '\ASend-MailMessage:') ($values[$_]) @sv}
+	Set-Variable PSEmailServer ($values['Send-MailMessage:SmtpServer']) @sv
 }
 else
 {
-	Write-Verbose 'Delivery method is not configured as Network'
+	try{[void][Configuration.ConfigurationManager]}catch{Add-Type -as System.Configuration}
+	$smtp = [Configuration.ConfigurationManager]::GetSection('system.net/mailSettings/smtp')
+	if($smtp.From) { Set-ParameterDefault.ps1 Send-MailMessage From $smtp.From @sv }
+	if($smtp.DeliveryMethod -eq 'Network' -and $smtp.Network)
+	{
+		if($smtp.Network.Host) {Set-ParameterDefault.ps1 Send-MailMessage SmtpServer $smtp.Network.Host @sv}
+		if($smtp.Network.EnableSsl) {Set-ParameterDefault.ps1 Send-MailMessage UseSsl $smtp.Network.EnableSsl @sv}
+	}
+	else
+	{
+		Write-Verbose 'Delivery method is not configured as Network'
+	}
+	Set-Variable PSEmailServer $smtp.Network.Host @sv
 }
-Set-Variable PSEmailServer $smtp.Network.Host @sv
