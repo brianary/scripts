d6f4156725476bb8e09452a5fd4d58537e0fda0d
Brian Lalonde <brianary@gmail.com>
2022-11-29 20:39:20 -0800

    ✅ Add tests for Add-ScopeLevel.ps1

---
+++ test/Add-ScopeLevel.Tests.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+Tests conversion of a scope level to account for another call stack level.
+#>
+
+Describe 'Add-ScopeLevel' -Tag Add-ScopeLevel {
+	BeforeAll {
+		if(!(Get-Module -List PSScriptAnalyzer)) {Install-Module PSScriptAnalyzer -Force}
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+	}
+	Context 'Script style' -Tag Style {
+		It "Should follow best practices for style" {
+			Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\Add-ScopeLevel.ps1" -Severity Warning |
+				Should -HaveCount 0 -Because 'there should be no style warnings'
+			Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\Add-ScopeLevel.ps1" -Severity Error |
+				Should -HaveCount 0 -Because 'there should be no style errors'
+		}
+	}
+	Context 'Convert a scope level to account for another call stack level.' -Tag Example {
+		It 'Calculates local scope' {
+			Add-ScopeLevel.ps1 Local |Should -BeExactly '1'
+		}
+		It 'Calculates a numeric scope' {
+			1..8 |ForEach-Object {Add-ScopeLevel.ps1 $_ |Should -BeExactly "$($_+1)"}
+		}
+		It 'Calulates global scope' {
+			Add-ScopeLevel.ps1 Global |Should -BeExactly 'Global'
+		}
+	}
+}
