7661dd4975532b95821a0a65f01a7f94ebc487dc
Brian Lalonde <brianary@gmail.com>
2022-12-14 20:49:10 -0800

    ✅ Add env var tests

--- Compress-EnvironmentVariables.ps1
+++ Compress-EnvironmentVariables.ps1
@@ -2,6 +2,20 @@
 .SYNOPSIS
 Replaces each of the longest matching parts of a string with an embedded environment variable with that value.
 
+.DESCRIPTION
+This provides a way to un-expand environment variables in a string, to make it smaller,
+or more portable, or to make the use of specific paths more understandable.
+
+The output of this script can be passed to [Environment]::ExpandEnvironmentVariables()
+to get the original string (provided the variable values are the same).
+
+.INPUTS
+System.String containing text to un-expand environment variables in.
+
+.OUTPUTS
+System.String with any possible environment varibles in place of their expanded text,
+which could be passed to [Environment]::ExpandEnvironmentVariables().
+
 .FUNCTIONALITY
 EnvironmentVariables
 
@@ -12,19 +26,20 @@ Compress-EnvironmentVariables.ps1 'C:\Program Files\Git\bin\git.exe'
 #>
 
 #Requires -Version 3
-[CmdletBinding()] Param(
+[CmdletBinding()][OutputType([string])] Param(
 # The string to generalize with environment variable substitution.
 [Parameter(Position=0, Mandatory=$true,ValueFromPipeline=$true)][string] $Value
 )
 Begin
 {
-	$envs = Get-ChildItem env: |where Name -notin 'ProgramW6432','ALLUSERSPROFILE','PROCESSOR_LEVEL','NUMBER_OF_PROCESSORS'
-	function Get-EnvMatch([string]$value)
+	$envs = Get-ChildItem env: |
+		Where-Object Name -notin 'ProgramW6432','ALLUSERSPROFILE','PROCESSOR_LEVEL','NUMBER_OF_PROCESSORS'
+	function Get-EnvMatch([string] $Text)
 	{
 		return $envs |
-			where {$Value.IndexOf($_.Value,[StringComparison]::CurrentCultureIgnoreCase) -gt -1} |
-			sort {$_.Value.Length} -Descending |
-			select -First 1
+			Where-Object {$Text.IndexOf($_.Value,[StringComparison]::CurrentCultureIgnoreCase) -gt -1} |
+			Sort-Object {$_.Value.Length} -Descending |
+			Select-Object -First 1
 	}
 }
 Process
---
+++ test/Compress-EnvironmentVariables.Tests.ps1
@@ -0,0 +1,39 @@
+﻿<#
+.SYNOPSIS
+Tests replacing each of the longest matching parts of a string with an embedded environment variable with that value.
+#>
+
+Describe 'Compress-EnvironmentVariables' -Tag Compress-EnvironmentVariables {
+	BeforeAll {
+		if(!(Get-Module -List PSScriptAnalyzer)) {Install-Module PSScriptAnalyzer -Force}
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		$ScriptName = Join-Path $scriptsdir Compress-EnvironmentVariables.ps1
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+	}
+	Context 'Comment-based help' -Tag CommentHelp {
+		It "Should produce help object" {
+			Get-Help $ScriptName |Should -Not -BeOfType string `
+				-Because 'Get-Help should not fall back to the default help string'
+		}
+	}
+	Context 'Script style' -Tag Style {
+		It "Should follow best practices for style" {
+			Invoke-ScriptAnalyzer -Path $ScriptName -Severity Warning |
+				ForEach-Object {$_.Severity,$_.ScriptName,$_.Line,$_.Column,$_.RuleName,$_.Message -join ':'} |
+				Should -BeExactly $null -Because 'there should be no style warnings'
+			Invoke-ScriptAnalyzer -Path $ScriptName -Severity Error |
+				ForEach-Object {$_.Severity,$_.ScriptName,$_.Line,$_.Column,$_.RuleName,$_.Message -join ':'} |
+				Should -BeExactly $null -Because 'there should be no style errors'
+		}
+	}
+	Context 'Replaces each of the longest matching parts of a string with an embedded environment variable with that value' -Tag EnvironmentVariables {
+		It "For '<Value>', returns '<Result>'" -TestCases @(
+			@{ Value ="[$env:APPDATA]"; Result = '[%APPDATA%]' }
+			@{ Value ="[$env:COMPUTERNAME]"; Result = '[%COMPUTERNAME%]' }
+			@{ Value ="$env:TEMP\tempdata"; Result = '%TEMP%\tempdata' }
+		) {
+			Param([string] $Value, [string] $Result)
+			Compress-EnvironmentVariables.ps1 $Value |Should -Be $Result
+		}
+	}
+}
