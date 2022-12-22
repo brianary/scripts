fa0241fe19f99809c4d869c1689737d818801fab
Brian Lalonde <brianary@gmail.com>
2022-12-21 20:24:29 -0800

    ✨ Add script to  copy HTML tables

---
+++ Copy-Html.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+Copies objects as an HTML table.
+
+.INPUTS
+System.Management.Automation.PSObject to be turned into a table row.
+
+.LINK
+Format-HtmlDataTable.ps1
+
+.LINK
+ConvertTo-SafeEntities.ps1
+
+.LINK
+Invoke-WindowsPowerShell.ps1
+
+.EXAMPLE
+Get-PSDrive |Copy-Html.ps1 Name,Description
+
+Copies an HTML table with two columns to the clipboard as formatted text
+that can be pasted into emails or other formatted documents.
+#>
+
+#Requires -Version 7
+[CmdletBinding()][OutputType([void])] Param(
+# Columns to include in the copied table.
+[Parameter(Position=0)][array] $Property,
+# The objects to turn into table rows.
+[Parameter(ValueFromPipeline=$true)][psobject] $InputObject
+)
+End
+{
+	$input |
+		Select-Object -Property $Property |
+		ConvertTo-Html -Fragment |
+		Format-HtmlDataTable.ps1 |
+		ConvertTo-SafeEntities.ps1 |
+		Set-Clipboard
+	Invoke-WindowsPowerShell.ps1 { Get-Clipboard |Set-Clipboard -AsHtml }
+}
