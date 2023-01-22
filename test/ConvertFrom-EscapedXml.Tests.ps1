853b57d2a316e067612a59e90f07c40cc95bb68f
Brian Lalonde <brianary@gmail.com>
2023-01-21 17:43:37 -0800

    ✅ Add escaped XML parsing tests

---
+++ test/ConvertFrom-EscapedXml.Tests.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.SYNOPSIS
+Tests parsing escaped XML into XML and serialization.
+#>
+
+Describe 'ConvertFrom-EscapedXml' -Tag ConvertFrom-EscapedXml {
+	BeforeAll {
+		if(!(Get-Module -List PSScriptAnalyzer)) {Install-Module PSScriptAnalyzer -Force}
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		$ScriptName = Join-Path $scriptsdir ConvertFrom-EscapedXml.ps1
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+	}
+	Context 'Comment-based help' -Tag CommentHelp {
+		It "Should produce help object" {
+			Get-Help $ScriptName |Should -Not -BeOfType string `
+				-Because 'Get-Help should not fall back to the default help string'
+		}
+	}
+	Context 'Script style' -Tag Style {
+		It "Should follow best practices for style" {
+			Invoke-ScriptAnalyzer -Path $ScriptName -Severity Warning |
+				ForEach-Object {$_.Severity,$_.ScriptName,$_.Line,$_.Column,$_.RuleName,$_.Message -join ':'} |
+				Should -BeExactly $null -Because 'there should be no style warnings'
+			Invoke-ScriptAnalyzer -Path $ScriptName -Severity Error |
+				ForEach-Object {$_.Severity,$_.ScriptName,$_.Line,$_.Column,$_.RuleName,$_.Message -join ':'} |
+				Should -BeExactly $null -Because 'there should be no style errors'
+		}
+	}
+	Context 'Parse escaped XML into XML and serialize it' -Tag ConvertFromEscapedXml,Convert,ConvertFrom,EscapedXml,Xml {
+		It "Should convert '<Input>' into '<Output>'" -TestCases @(
+			@{ Value = '&lt;x /&gt;'; Result = '<x />' }
+			@{ Value = '&lt;a href=&quot;http://example.org&quot;&gt;link&lt;/a&gt;'
+				Result = '<a href="http://example.org">link</a>' }
+		) {
+			Param([string]$Value,[string]$Result)
+			ConvertFrom-EscapedXml.ps1 $Value |Should -BeExactly $Result -Because 'input parameter should work'
+			$Value |ConvertFrom-EscapedXml.ps1 |Should -BeExactly $Result -Because 'input pipeline should work'
+		}
+	}
+
+}
