2b364e4b5b4af4aa313ff03537938bfd6cc9de37
Brian Lalonde <brianary@gmail.com>
2023-02-17 13:10:06 -0800

    ♻️ Rewrite visible string output

--- Write-VisibleString.ps1
+++ Write-VisibleString.ps1
@@ -8,43 +8,71 @@ System.Object to serialize with nonprintable characters made visible as a hex pa
 .EXAMPLE
 Write-VisibleString.ps1 "a`tb`nc"
 
-a 09 b 0a c
+a 09 b 0A c
+(Formatting is not displayed in help.)
 #>
 
-#Requires -Version 3
+#Requires -Version 7
 [CmdletBinding()][OutputType([void])] Param(
 # The string to show.
 [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][object] $InputObject,
-# The color to use for nonprintable chars.
-[ConsoleColor] $AltColor = 'DarkYellow',
+# Parse Runes from the string rather than Chars.
+[switch] $AsRunes,
 # Print control characters as control picture symbols rather than hex values.
-[switch] $AsSymbols
+[switch] $UseSymbols
 )
-$formatHex = {if([int]$_ -gt 0x100){'{0:X4}' -f [int]$_}else{'{0:X2}' -f [int]$_}}
-function Write-Strings([string[]] $String)
+Begin
 {
-	if(!$String) {return}
-	[char] $c = $String[0][0]
-	if(![char]::IsControl($c) -and ![char]::IsWhiteSpace($c) -and
-		![char]::IsLowSurrogate($c) -and ![char]::IsHighSurrogate($c))
+	$symbolStart = $PSStyle.Reverse
+	$symbolEnd = $PSStyle.ReverseOff
+	$shortHexStart = '{0} {1}{2}' -f $PSStyle.Hidden,$PSStyle.HiddenOff,$PSStyle.Reverse
+	$shortHexEnd = '{0}{1} {2}' -f $PSStyle.ReverseOff,$PSStyle.Hidden,$PSStyle.HiddenOff
+	$longHexStart = '{0} {1}{2}{3}' -f $PSStyle.Hidden,$PSStyle.HiddenOff,$PSStyle.Reverse,$PSStyle.Bold
+	$longHexEnd = '{0}{1}{2} {3}' -f $PSStyle.BoldOff,$PSStyle.ReverseOff,$PSStyle.Hidden,$PSStyle.HiddenOff
+	filter Write-Char([Parameter(ValueFromPipeline)][int] $Value)
 	{
-		Write-Info.ps1 $String[0] -NoNewline
-	}
-	else
-	{
-		if($AsSymbols -and ([int]$c) -le 0x20)
+		if(![char]::IsControl($Value) -and ![char]::IsWhiteSpace($Value) -and
+			![char]::IsLowSurrogate($Value) -and ![char]::IsHighSurrogate($Value))
 		{
-			$Local:OFS = ''
-			Write-Info.ps1 ([string]($String[0].ToCharArray() |foreach {[char](0x2400+[int]$_)})) `
-				-NoNewline -ForegroundColor $AltColor
+			return [char]$Value
+		}
+		elseif($UseSymbols -and $Value -le 0x20)
+		{
+			return '{0}{1}{2}' -f $symbolStart,[char](0x2400+$Value),$symbolEnd
+		}
+		elseif($Value -ge 0x100)
+		{
+			return '{0}{1:X4}{2}' -f $longHexStart,$Value,$longHexEnd
 		}
 		else
 		{
-			Write-Info.ps1 " $($String[0].GetEnumerator() |foreach $formatHex) " `
-				-NoNewline -ForegroundColor $AltColor
+			return '{0}{1:X2}{2}' -f $shortHexStart,$Value,$shortHexEnd
 		}
 	}
-	if($String.Length -gt 1) {Write-Strings ($String[1..($String.Length-1)])}
-	else {Write-Info.ps1}
+	filter Write-Rune([Parameter(ValueFromPipelineByPropertyName)][int] $Value,
+		[Parameter(ValueFromPipelineByPropertyName)][bool] $IsAscii,
+		[Parameter(ValueFromPipelineByPropertyName)][bool] $IsBmp)
+	{
+		if($IsAscii -and ![char]::IsControl($Value) -and ![char]::IsWhiteSpace($Value))
+		{
+			return [char]$Value
+		}
+		elseif($UseSymbols -and $Value -le 0x20)
+		{
+			return '{0}{1}{2}' -f $symbolStart,[char](0x2400+$Value),$symbolEnd
+		}
+		elseif($Value -ge 0x100)
+		{
+			return '{0}{1:X4}{2}' -f $longHexStart,$Value,$longHexEnd
+		}
+		else
+		{
+			return '{0}{1:X2}{2}' -f $shortHexStart,$Value,$shortHexEnd
+		}
+	}
 }
-Write-Strings ([convert]::ToString($InputObject) -split '((?:\p{C}+|\p{Z}+))')
+Process
+{
+	if($AsRunes) {[convert]::ToString($InputObject).EnumerateRunes() |Write-Rune |Out-String -NoNewline |Write-Info.ps1}
+	else {[convert]::ToString($InputObject).ToCharArray() |Write-Char |Out-String -NoNewline |Write-Info.ps1}
+}
