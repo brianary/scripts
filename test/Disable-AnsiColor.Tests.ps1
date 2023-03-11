d0d861e9ce9082ae1df45fe63d8cb815854b8078
Brian Lalonde <brianary@gmail.com>
2023-03-10 16:23:11 -0800

    ✅ Add ANSI color tests

---
+++ test/Disable-AnsiColor.Tests.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+Tests disabling ANSI terminal colors.
+#>
+
+Describe 'Disable-AnsiColor' -Tag Disable-AnsiColor {
+	BeforeAll {
+		if(!(Get-Module -List PSScriptAnalyzer)) {Install-Module PSScriptAnalyzer -Force}
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+	}
+	Context 'Disables ANSI terminal colors' -Tag DisableAnsiColor,Disable,AnsiColor {
+		It "Sets OutputRendering" {
+			Param([string] $OutputRendering)
+			$PSStyle.OutputRendering = 'Ansi'
+			Disable-AnsiColor.ps1
+			$PSStyle.OutputRendering |Should -Be 'PlainText'
+			Disable-AnsiColor.ps1 -HostOnly
+			$PSStyle.OutputRendering |Should -Be 'Host'
+		}
+	}
+}
---
+++ test/Enable-AnsiColor.Tests.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+Tests Enables ANSI terminal colors.
+#>
+
+Describe 'Enable-AnsiColor' -Tag Enable-AnsiColor {
+	BeforeAll {
+		if(!(Get-Module -List PSScriptAnalyzer)) {Install-Module PSScriptAnalyzer -Force}
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+	}
+	Context 'Enables ANSI terminal colors.' -Tag EnableAnsiColor,Enable,AnsiColor {
+		It "Sets OutputRendering" {
+			Param([string] $OutputRendering)
+			$PSStyle.OutputRendering = 'PlainText'
+			Enable-AnsiColor.ps1 -HostOnly
+			$PSStyle.OutputRendering |Should -Be 'Host'
+			Enable-AnsiColor.ps1
+			$PSStyle.OutputRendering |Should -Be 'Ansi'
+		}
+	}
+}
