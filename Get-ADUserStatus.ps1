78062a9195914e50c5b9c6b7ae0f541004d8cb6a
Brian Lalonde <brianary@gmail.com>
2023-04-18 08:09:31 -0700

    ⚡📝 PSC supports AD! Add docs

--- Get-ADUserStatus.ps1
+++ Get-ADUserStatus.ps1
@@ -3,19 +3,44 @@
 Returns the current login properties of an ActiveDirectory user.
 
 .LINK
+https://learn.microsoft.com/powershell/module/activedirectory/get-aduser
+
+.LINK
 Invoke-WindowsPowerShell.ps1
 
 .LINK
 Get-ADUser
+
+.EXAMPLE
+Get-ADUserStatus.ps1 alans
+
+AccountExpirationDate  :
+AccountExpires         : 9223372036854775807
+AccountLockoutTime     :
+BadLogonCount          : 0
+BadPwdCount            : 0
+DistinguishedName      : CN=Alan Smithee,OU=Directors,DC=example,DC=local
+Enabled                : True
+GivenName              : Alan
+LastBadPasswordAttempt : 04/17/2023 2:16:20 PM
+LastLogonDate          : 04/12/2023 5:07:55 PM
+LockedOut              : False
+Name                   : Alan Smithee
+ObjectClass            : user
+ObjectGUID             : 0a2e6b9c-83d5-466b-b45b-69d6fe626b08
+PasswordExpired        : False
+PasswordLastSet        : 04/18/2023 12:01:01 AM
+PwdLastSet             : 133262748613029225
+SamAccountName         : alans
+SID                    : S-1-5-21-3351665499-1662801859-681883470-29151
+Surname                : Smithee
+UserPrincipalName      : alans@example.local
 #>
 
 #Requires -Version 7
-[CmdletBinding()] Param(
-[Parameter(Position=0,Mandatory=$true)][string] $Identity
+#Requires -Modules ActiveDirectory
+[CmdletBinding()][OutputType([Microsoft.ActiveDirectory.Management.ADUser])] Param(
+[Parameter(Position=0,Mandatory=$true)][Microsoft.ActiveDirectory.Management.ADUser] $Identity
 )
-Invoke-WindowsPowerShell.ps1 {
-	Param([string] $Identity)
-	Import-Module ActiveDirectory
-	Get-ADUser -Identity $Identity -properties AccountExpirationDate, AccountExpires, AccountLockoutTime, BadLogonCount,
-	PadPwdCount, LastBadPasswordAttempt, LastLogonDate, LockedOut, PasswordExpired, PasswordLastSet, PwdLastSet
-} -BlockArgs $Identity
+Get-ADUser -Identity $Identity -Properties AccountExpirationDate, AccountExpires, AccountLockoutTime, BadLogonCount,
+	BadPwdCount, LastBadPasswordAttempt, LastLogonDate, LockedOut, PasswordExpired, PasswordLastSet, PwdLastSet
