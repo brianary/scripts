1701b97006a786152be4734e1e47026cf76b5699
Brian Lalonde <brianary@gmail.com>
2023-07-02 11:31:59 -0700

    👽️ Disconinue using unsupported alias "cup"

--- Update-Everything.cmd
+++ Update-Everything.cmd
@@ -1,5 +1,5 @@
 taskkill /im powershell.exe /f
 taskkill /im pwsh.exe /f
 taskkill /im WindowsTerminal.exe /f
-cup powershell powershell-core microsoft-windows-terminal -y
+choco upgrade powershell powershell-core microsoft-windows-terminal -y
 pwsh -nol -nop -noni -file %~dpn0.ps1
