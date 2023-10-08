3addef650ecdb722b633b7d26c2d4ba05969b195
Brian Lalonde <brianary@gmail.com>
2023-10-07 21:15:51 -0700

    ⚗️ Some more testing of Unicode parsing

--- UnicodeData.fsx
+++ UnicodeData.fsx
@@ -5,13 +5,15 @@ Parse Unicode data
 Experiment with CSV type provider to read Unicode data.
 *)
 #r "nuget: FSharp.Data"
-open System
 open System.IO
 open FSharp.Data
 [<Literal>]
 let pwd = __SOURCE_DIRECTORY__
-type UnicodeData = CsvProvider<"./test/UnicodeData-sample.txt", ";", ResolutionFolder=pwd>
-let u = UnicodeData.Load((Path.Combine(pwd, "./test/UnicodeData-sample.txt"))).Cache()
+[<Literal>]
+let headers = "Value,Name,Catgory,CombiningClass,BidirectionalCategory,DecompositionMapping,"
+            + "DecimalDigitValue,DigitValue,NumericValue,Mirrored,OldName,Comment,Upper,Lower,Title"
+type UnicodeData = CsvProvider<"./test/UnicodeData.txt", ";", ResolutionFolder=pwd, HasHeaders=false, Schema=headers>
+let u = UnicodeData.Load((Path.Combine(pwd, "./test/UnicodeData.txt"))).Cache()
 let first = u.Rows |> Seq.head
 first
 let asterisk = u.Rows |> Seq.find (fun x -> x.Name = "ASTERISK")
