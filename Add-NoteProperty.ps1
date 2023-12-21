2d3e7be4e90979ff285b935247e738abc688e49c
Brian Lalonde <brianary@gmail.com>
2023-12-21 14:27:30 -0800

    ✨ Support adding multiple properties; update Unicode data

--- Add-NoteProperty.ps1
+++ Add-NoteProperty.ps1
@@ -23,41 +23,52 @@ Add-Member
 .EXAMPLE
 Get-ChildItem Get-*.ps1 |Add-NoteProperty.ps1 Size {Format-ByteUnits.ps1 $Length -Precision 1} -Properties Length -PassThru |Format-Table Size,Name -AutoSize
 
-Size   Name
-----   ----
-8.1KB  Get-AspNetEvents.ps1
-840    Get-AssemblyFramework.ps1
-38.3KB Get-CharacterDetails.ps1
-1.1KB  Get-ClassicAspEvents.ps1
-1.3KB  Get-CommandPath.ps1
-1.2KB  Get-ConfigConnectionStringBuilders.ps1
-4.9KB  Get-ConsoleColors.ps1
-1.4KB  Get-ContentSecurityPolicy.ps1
-617    Get-Dns.ps1
-2.4KB  Get-EnumValues.ps1
-6KB    Get-IisLog.ps1
-1.9KB  Get-LibraryVulnerabilityInfo.ps1
-2.7KB  Get-DotNetFrameworkVersions.ps1
-969    Get-RepoName.ps1
-3.3KB  Get-SslDetails.ps1
-4.2KB  Get-SystemDetails.ps1
-6.8KB  Get-TypeAccelerators.ps1
-1.2KB  Get-XmlNamespaces.ps1
+|  Size Name
+|  ---- ----
+| 8.1KB Get-AspNetEvents.ps1
+|   840 Get-AssemblyFramework.ps1
+|38.3KB Get-CharacterDetails.ps1
+| 1.1KB Get-ClassicAspEvents.ps1
+| 1.3KB Get-CommandPath.ps1
+| 1.2KB Get-ConfigConnectionStringBuilders.ps1
+| 4.9KB Get-ConsoleColors.ps1
+| 1.4KB Get-ContentSecurityPolicy.ps1
+|   617 Get-Dns.ps1
+| 2.4KB Get-EnumValues.ps1
+|   6KB Get-IisLog.ps1
+| 1.9KB Get-LibraryVulnerabilityInfo.ps1
+| 2.7KB Get-DotNetFrameworkVersions.ps1
+|   969 Get-RepoName.ps1
+| 3.3KB Get-SslDetails.ps1
+| 4.2KB Get-SystemDetails.ps1
+| 6.8KB Get-TypeAccelerators.ps1
+| 1.2KB Get-XmlNamespaces.ps1
+
+.EXAMPLE
+Get-ChildItem Get-*.ps1 |Add-NoteProperty.ps1 @{Size={Format-ByteUnits.ps1 $_.Length -Precision 1};Ext={$_ |Split-Path -Extension};Hat='Y'} -PassThru |Format-Table Size,Name,Ext,Hat -AutoSize
+
+|  Size Name                                   Ext  Hat
+|  ---- ----                                   ---  ---
+|   928 Get-ADServiceAccountInfo.ps1           .ps1 Y
+| 1.9KB Get-ADUserStatus.ps1                   .ps1 Y
+| 8.1KB Get-AspNetEvents.ps1                   .ps1 Y
+| 1.1KB Get-AssemblyFramework.ps1              .ps1 Y
+| 2.9KB Get-CachedCredential.ps1               .ps1 Y
+|34.5KB Get-CharacterDetails.ps1               .ps1 Y
 #>
 
 [CmdletBinding()][OutputType([psobject])] Param(
 # The name of the NoteProperty to add to the object.
-[Parameter(Position=0,Mandatory=$true)][string] $Name,
+[Parameter(ParameterSetName='NameValue',Position=0,Mandatory=$true)][string] $Name,
 # The expression to use to set the value of the NoteProperty.
-[Parameter(Position=1,Mandatory=$true)][ScriptBlock] $Value,
+[Parameter(ParameterSetName='NameValue',Position=1,Mandatory=$true)][ScriptBlock] $Value,
+# A hashtable mapping names of new properties to script blocks generating the values of those properties.
+[Parameter(ParameterSetName='NewProperties',Position=0,Mandatory=$true)][hashtable] $NewProperties,
 # Properties of the input object to include as variables in the script block scope.
 [Alias('Import')][string[]] $Properties = @(),
 # The object to add the NoteProperty to.
 [Parameter(ValueFromPipeline=$true,Mandatory=$true)][PSObject] $InputObject,
-<#
-Returns the object with the NoteProperty added.
-Normally there is no output.
-#>
+# Returns the object with the NoteProperty added. Normally there is no output.
 [switch] $PassThru,
 # Overwrite an existing property.
 [switch] $Force
@@ -79,7 +90,23 @@ Process
 				ForEach-Object {New-Object psvariable $_.Name,$_.Value}
 		}
 	}
-	$v = $Value.InvokeWithContext($null,$context,$null) |Select-Object -First 1
-	Add-Member -InputObject $InputObject -MemberType NoteProperty -Name $Name `
-		-Value $v -PassThru:$PassThru -Force:$Force
+	switch($PSCmdlet.ParameterSetName)
+	{
+		NameValue
+		{
+			$v = $Value.InvokeWithContext($null,$context,$null) |Select-Object -First 1
+			Add-Member -InputObject $InputObject -MemberType NoteProperty -Name $Name `
+				-Value $v -PassThru:$PassThru -Force:$Force
+		}
+		NewProperties
+		{
+			$NewProperties.Keys |ForEach-Object {
+				$v = $NewProperties[$_]
+				if($v -is [scriptblock]) {$v = $v.InvokeWithContext($null,$context,$null) |Select-Object -First 1}
+				Add-Member -InputObject $InputObject -MemberType NoteProperty -Name $_ -Value $v -Force:$Force
+			}
+			if($PassThru) {Write-Output $InputObject}
+		}
+		default {Stop-ThrowError.ps1 'Unknown parameter set' -TargetObject $PSBoundParameters}
+	}
 }
--- test/Add-NoteProperty.Tests.ps1
+++ test/Add-NoteProperty.Tests.ps1
@@ -14,9 +14,20 @@ Describe 'Add-NoteProperty' -Tag Add-NoteProperty -Skip:$skip {
 	}
 	Context 'Add a calculated property value' -Tag AddNoteProperty,Add,NoteProperty {
 		It "Should add a property with a static value calculated when added" {
-			$value = [pscustomobject]@{x=8} |Add-NoteProperty.ps1 pow {[math]::Log2($_.x)} -PassThru
+			$value = [pscustomobject]@{x=8} |Add-NoteProperty.ps1 bits {[math]::Log2($_.x)} -PassThru
 			$value.x = 16 # this should not change the pow property
-			$value.pow |Should -Be 3 -Because 'the pow property value should have been determined only when added'
+			$value.bits |Should -Be 3 -Because 'the bits property value should have been determined only when added'
+		}
+		It "Should add multiple properties with a mix of value types" {
+			$value = [pscustomobject]@{x=8} |Add-NoteProperty.ps1 @{
+				bits = {[math]::Log2($_.x)}
+				binary = {'{0:b}' -f $_.x}
+				isNumeric = $true
+			} -PassThru
+			$value.x = 16 # this should not change the pow property
+			$value.bits |Should -Be 3 -Because 'the bits property value should be statically evaluated'
+			$value.binary |Should -Be '1000' -Because 'the binary property should be statically evaluated'
+			$value.isNumeric |Should -BeTrue -Because 'the isNumeric property should be true'
 		}
 	}
 }
