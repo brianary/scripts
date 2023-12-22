a8e06f8d8ac5792fe652ee41f16ed1e0ee5a1be9
Brian Lalonde <brianary@gmail.com>
2023-12-21 21:06:42 -0800

    ✅ Fix note property test

--- test/Add-NoteProperty.Tests.ps1
+++ test/Add-NoteProperty.Tests.ps1
@@ -21,12 +21,12 @@ Describe 'Add-NoteProperty' -Tag Add-NoteProperty -Skip:$skip {
 		It "Should add multiple properties with a mix of value types" {
 			$value = [pscustomobject]@{x=8} |Add-NoteProperty.ps1 @{
 				bits = {[math]::Log2($_.x)}
-				binary = {'{0:b}' -f $_.x}
+				format = {'<{0:X}>' -f ($_.x*4)}
 				isNumeric = $true
 			} -PassThru
 			$value.x = 16 # this should not change the pow property
 			$value.bits |Should -Be 3 -Because 'the bits property value should be statically evaluated'
-			$value.binary |Should -Be '1000' -Because 'the binary property should be statically evaluated'
+			$value.format |Should -Be '<20>' -Because 'the binary property should be statically evaluated'
 			$value.isNumeric |Should -BeTrue -Because 'the isNumeric property should be true'
 		}
 	}
