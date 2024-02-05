a795dc2f5a3dfd59d7cdeeaa90f53e74accc8821
Brian Lalonde <brianary@gmail.com>
2024-02-04 19:25:11 -0800

    ✅ Add tests

---
+++ test/Add-NotebookCell.Tests.ps1
@@ -0,0 +1,39 @@
+﻿<#
+.SYNOPSIS
+Tests running within a Polyglot Notebook, appending a cell to it.
+#>
+
+$basename = "$(($MyInvocation.MyCommand.Name -split '\.',2)[0])."
+$skip = !(Test-Path .changes -Type Leaf) ? $false :
+	!@(Get-Content .changes |Get-Item |Select-Object -ExpandProperty Name |Where-Object {$_.StartsWith($basename)})
+if($skip) {Write-Information "No changes to $basename" -infa Continue}
+Describe 'Add-NotebookCell' -Tag Add-NotebookCell -Skip:$skip {
+	BeforeAll {
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+		Add-Type -TypeDefinition @'
+using System.Collections.Generic;
+public record SendEditableCode(string Language, string Content);
+public static class Kernel
+{
+    public class RootMock
+    {
+        public SendEditableCode Sent = new SendEditableCode("", "");
+        public void SendAsync(SendEditableCode command) {Sent = command;}
+    }
+    public static RootMock Root = new RootMock();
+}
+'@
+	}
+	Context 'When run within a Polyglot Notebook, appends a cell to it' -Tag AddNoteboodCell,Add,NotebookCell,Notebook {
+		It "Adding language '<Language>' code '<Code>' should happen" -TestCases @(
+			@{ Language = 'sql'; Code = "select * from products;" }
+			@{ Language = 'mermaid'; Code = "flowchart LR`nA -->B" }
+		) {
+			Param([string] $Language, [string] $Code)
+			$Code |Add-NotebookCell.ps1 -Language $Language
+			[Kernel]::Root.Sent.Language |Should -BeExactly $Language
+			[Kernel]::Root.Sent.Content.TrimEnd() |Should -BeExactly $Code
+		}
+	}
+}
---
+++ test/ConvertFrom-EpochTime.Tests.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+Tests converting an integer Unix (POSIX) time (seconds since Jan 1, 1970) into a DateTime value.
+#>
+
+$basename = "$(($MyInvocation.MyCommand.Name -split '\.',2)[0])."
+$skip = !(Test-Path .changes -Type Leaf) ? $false :
+	!@(Get-Content .changes |Get-Item |Select-Object -ExpandProperty Name |Where-Object {$_.StartsWith($basename)})
+if($skip) {Write-Information "No changes to $basename" -infa Continue}
+Describe 'ConvertFrom-EpochTime' -Tag ConvertFrom-EpochTime -Skip:$skip {
+	BeforeAll {
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+	}
+	Context 'Converts an integer Unix (POSIX) time (seconds since Jan 1, 1970) into a DateTime value' `
+		-Tag ConvertFromEpochTime,ConvertFrom,Convert,EpochTime {
+		It "Epoch time '<EpochTime>' is converted to '<Result>'" -TestCases @(
+			@{ EpochTime = 946684800; Result = '2000-01-01' }
+			@{ EpochTime = 1012615322; Result = '2002-02-02T02:02:02' }
+			@{ EpochTime = 1645568542; Result = '2022-02-22T22:22:22' }
+			@{ EpochTime = 0; Result = '1970-01-01' }
+			@{ EpochTime = 663486600; Result = '1991-01-10T05:50' }
+		) {
+			Param([int] $EpochTime, [datetime] $Result)
+			$EpochTime |ConvertFrom-EpochTime.ps1 |Should -BeExactly $Result
+		}
+	}
+
+}
