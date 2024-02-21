1b9bc9045b5368e67e2b2814ee21b78c3b64785e
Brian Lalonde <brianary@gmail.com>
2024-02-20 21:00:35 -0800

    ✅ Add more export tests

---
+++ test/Export-EdgeKeywords.Tests.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+Tests returning the configured search keywords from an Edge SQLite file.
+#>
+
+$basename = "$(($MyInvocation.MyCommand.Name -split '\.',2)[0])."
+$skip = !(Test-Path .changes -Type Leaf) ? $false :
+	!@(Get-Content .changes |Get-Item |Select-Object -ExpandProperty Name |Where-Object {$_.StartsWith($basename)})
+if($skip) {Write-Information "No changes to $basename" -infa Continue}
+Describe 'Export-EdgeKeywords' -Tag Export-EdgeKeywords -Skip:$skip {
+	BeforeAll {
+		if(!(Get-Module -List PSSQLite)) {Install-Module PSSQLite -Force}
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+		Mock Invoke-SqliteQuery {}
+	}
+	Context 'Returns the configured search keywords from an Edge SQLite file' -Tag ExportEdgeKeywords,Export,EdgeKeywords {
+		It "Queries the default Edge SQLLite file" {
+			Export-EdgeKeywords.ps1
+			Assert-MockCalled -CommandName Invoke-SqliteQuery -Times 1
+		}
+	}
+}
---
+++ test/Export-InstalledPackages.Tests.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.SYNOPSIS
+Tests exporting the list of packages installed by various tools.
+#>
+
+$basename = "$(($MyInvocation.MyCommand.Name -split '\.',2)[0])."
+$skip = !(Test-Path .changes -Type Leaf) ? $false :
+	!@(Get-Content .changes |Get-Item |Select-Object -ExpandProperty Name |Where-Object {$_.StartsWith($basename)})
+if($skip) {Write-Information "No changes to $basename" -infa Continue}
+Describe 'Export-InstalledPackages' -Tag Export-InstalledPackages -Skip:$skip {
+	BeforeAll {
+		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
+		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
+		$installed = @{
+			PSModules = 'Get-Module'
+			WinGet = 'winget'
+			Chocolatey = 'choco'
+			Scoop = 'scoop'
+			Npm = 'npm'
+			DotNetTools = 'dotnet'
+			GitHubExtensions = 'gh'
+		}
+		@($installed.Keys) |Where-Object {!(Get-Command $_ -ErrorAction Ignore)} |ForEach-Object {$installed.Remove($_)}
+		function scoop {[pscustomobject]@{Name='Scoop'}}
+		Mock Get-Module {[pscustomobject]@{Name='PSModules'}}
+		Mock winget {'{Sources:{Packages:{PackageIdentifier:["WinGet"]}}}' |Out-File "$env:temp\winget.json"} -ErrorAction Ignore
+		Mock choco {'','Chocolatey',''} -ErrorAction Ignore
+		Mock scoop {[pscustomobject]@{Name='Scoop'}} -ErrorAction Ignore
+		Mock npm {'{dependencies:{Npm:true}}'} -ErrorAction Ignore
+		Mock dotnet {'','','DotNetTools version'} -ErrorAction Ignore
+		Mock gh {'- - GitHubExtensions -'} -ErrorAction Ignore
+	}
+	Context 'Exports the list of packages installed by various tools' `
+		-Tag ExportInstalledPackages,Export,InstalledPackages,Packages {
+		It "Queries the installed packages" {
+			$packages = Export-InstalledPackages.ps1
+			$installed.Values |ForEach-Object {Assert-MockCalled -CommandName $_ -Times 1}
+			if($packages.ContainsKey('ScoopBuckets')) {$packages.Remove('ScoopBuckets')}
+			$packages.Keys |ForEach-Object {$packages[$_] |Should -BeExactly $_}
+		}
+	}
+}
