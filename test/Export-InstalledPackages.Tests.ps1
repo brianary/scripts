db7fd16226727cb0da32c5aa3389340eba8eb237
Brian Lalonde <brianary@gmail.com>
2024-02-21 19:04:17 -0800

    ✅ Disable mocking scoop to try and fix test

--- test/Export-InstalledPackages.Tests.ps1
+++ test/Export-InstalledPackages.Tests.ps1
@@ -22,11 +22,11 @@ Describe 'Export-InstalledPackages' -Tag Export-InstalledPackages -Skip:$skip {
 		}
 		@($installed.Keys) |Where-Object {!(Get-Command $_ -ErrorAction Ignore)} |ForEach-Object {$installed.Remove($_)}
 		#function winget {'{Sources:{Packages:{PackageIdentifier:["WinGet"]}}}' |Out-File "$env:temp\winget.json"}
-		function scoop {[pscustomobject]@{Name='Scoop'}}
+		#function scoop {[pscustomobject]@{Name='Scoop'}}
 		Mock Get-Module {[pscustomobject]@{Name='PSModules'}}
 		#Mock winget {'{Sources:{Packages:{PackageIdentifier:["WinGet"]}}}' |Out-File "$env:temp\winget.json"} -ErrorAction Ignore
 		Mock choco {'','Chocolatey',''} -ErrorAction Ignore
-		Mock scoop {[pscustomobject]@{Name='Scoop'}} -ErrorAction Ignore
+		#Mock scoop {[pscustomobject]@{Name='Scoop'}} -ErrorAction Ignore
 		Mock npm {'{dependencies:{Npm:true}}'} -ErrorAction Ignore
 		Mock dotnet {'','','DotNetTools version'} -ErrorAction Ignore
 		Mock gh {'- - GitHubExtensions -'} -ErrorAction Ignore
