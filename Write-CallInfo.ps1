44311779f1359d734a4539e72ec34787d78c79a4
Brian Lalonde <brianary@gmail.com>
2024-03-01 12:36:21 -0800

    ✨ Add debugging script

---
+++ Write-CallInfo.ps1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+Prints caller name and parameters to the host for debugging purposes.
+#>
+
+#Requires -Version 3
+[CmdletBinding()] Param()
+(Get-Variable MyInvocation -ValueOnly -Scope 1).MyCommand.Name |Write-Host -ForegroundColor Cyan
+Get-Variable PSBoundParameters -ValueOnly -Scope 1 |ConvertTo-Json -Depth 100 |Write-Host -ForegroundColor DarkGray
