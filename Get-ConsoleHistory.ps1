ae3dd8c6734927f11ec9ff5907e887c167031d0b
Brian Lalonde <brianary@gmail.com>
2024-08-01 21:10:06 -0700

    ✨ Add more console history support

--- Get-ConsoleHistory.ps1
+++ Get-ConsoleHistory.ps1
@@ -12,17 +12,57 @@ System.Management.Automation.PSObject with these properties:
 Console
 
 .EXAMPLE
-Get-ConsoleHistory.ps1 |where CommandLine -like *readme*
+Get-ConsoleHistory.ps1 -Like *readme*
 
 Id CommandLine
 -- -----------
 30 gc .\README.md
 56 gc .\README.md
+
+.EXAMPLE
+Get-ConsoleHistory.ps1 -Id 30
+
+Id CommandLine
+-- -----------
+30 gc .\README.md
 #>
 
 #Requires -Version 3
-[CmdletBinding()] Param()
-
-$id = 0
-Get-Content $env:AppData\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt |
-	ForEach-Object {[pscustomobject]@{Id=++$id;CommandLine=$_}}
+[CmdletBinding()] Param(
+[Parameter(ParameterSetName='Id',Mandatory=$true)][int] $Id,
+[Parameter(ParameterSetName='Like',Mandatory=$true)][string] $Like,
+[Parameter(ParameterSetName='Match',Mandatory=$true)][string] $Match,
+[Parameter(ParameterSetName='All')][switch] $All
+)
+$history = Join-Path $env:AppData Microsoft Windows PowerShell PSReadline ConsoleHost_history.txt
+switch($PSCmdlet.ParameterSetName)
+{
+	Id
+	{
+		[pscustomobject]@{
+			Id = $Id
+			CommandLine = Get-Content $history -TotalCount $Id |Select-Object -Last 1
+		}
+	}
+	Like
+	{
+		$id = 0
+		(Get-Content $history) |Where-Object {$id++; $_ -like $Like} |ForEach-Object {[pscustomobject]@{
+			Id = $id
+			CommandLine = $_
+		}}
+	}
+	Match
+	{
+		Select-String -Pattern $Match -Path $history |ForEach-Object {[pscustomobject]@{
+			Id = $_.LineNumber
+			CommandLine = $_.Line
+		}}
+	}
+	default
+	{
+		$id = 0
+		Get-Content (Join-Path $env:AppData Microsoft Windows PowerShell PSReadline ConsoleHost_history.txt) |
+			ForEach-Object {[pscustomobject]@{Id=++$id;CommandLine=$_}}
+	}
+}
---
+++ Remove-ConsoleHistory.ps1
@@ -0,0 +1,81 @@
+<#
+.SYNOPSIS
+Removes an entry from the DOSKey-style console command history (up arrow or F8).
+
+.INPUTS
+System.String containing exact commands to remove.
+
+.FUNCTIONALITY
+Console
+
+.EXAMPLE
+Remove-ConsoleHistory.ps1 -Id 42
+
+Deletes the 42nd command from the history.
+
+.EXAMPLE
+Remove-ConsoleHistory.ps1 -Duplicates
+
+Deletes any repeated commands from the history.
+
+.EXAMPLE
+Remove-ConsoleHistory.ps1 -Like winget*
+
+Deletes any commands that start with "winget" from the history.
+#>
+
+#Requires -Version 7
+[CmdletBinding(ConfirmImpact='High',SupportsShouldProcess=$true)] Param(
+[Parameter(ParameterSetName='Id',Mandatory=$true)][int] $Id,
+[Parameter(ParameterSetName='CommandLine',Position=0,Mandatory=$true,ValueFromPipeline=$true)][string] $CommandLine,
+[Parameter(ParameterSetName='Like',Mandatory=$true)][string] $Like,
+[Parameter(ParameterSetName='Duplicates',Mandatory=$true)][switch] $Duplicates
+)
+Begin
+{
+	$history = Join-Path $env:AppData Microsoft Windows PowerShell PSReadline ConsoleHost_history.txt
+}
+Process
+{
+	switch($PSCmdlet.ParameterSetName)
+	{
+		Id
+		{
+			$cmd = Get-Content $history -TotalCount $Id |Select-Object -Last 1
+			if($PSCmdlet.ShouldProcess($cmd, 'remove'))
+			{
+				$i = 1
+				(Get-Content $history) |Where-Object {$i++ -ne $Id} |Out-File $history -Encoding utf8NoBOM
+				Write-Verbose "Removed '$cmd'"
+			}
+		}
+		CommandLine
+		{
+			[bool] $found = Get-Content $history |Where-Object {$_ -eq $CommandLine} |Select-Object -First 1
+			if($found -and $PSCmdlet.ShouldProcess($CommandLine, 'remove'))
+			{
+				(Get-Content $history) -ne $CommandLine |Out-File $history -Encoding utf8NoBOM
+				if($found) {Write-Verbose "Removed '$CommandLine'"}
+			}
+		}
+		Like
+		{
+			$cmd = (Get-Content $history) -like $Like
+			if($PSCmdlet.ShouldProcess("$($cmd.Count) commands matching '$Like'", 'remove'))
+			{
+				(Get-Content $history) -notlike $Like |Out-File $history -Encoding utf8NoBOM
+				$cmd |ForEach-Object {Write-Verbose "Removed '$_'"}
+			}
+		}
+		Duplicates
+		{
+			if($PSCmdlet.ShouldProcess('duplicate commands', 'remove'))
+			{
+				$before = (Get-Content $history).Count
+				(Get-Content $history) |Select-Object -Unique |Out-File $history -Encoding utf8NoBOM
+				$after = (Get-Content $history).Count
+				Write-Verbose "Removed $($before - $after) duplicate entries"
+			}
+		}
+	}
+}
