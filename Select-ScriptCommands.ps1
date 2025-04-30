07e0c07bab3ad89b8a879fbce5d61322b618ce90
Brian Lalonde <brianary@gmail.com>
2025-04-29 21:43:34 -0700

    ✨ Add script to return commands within a script file

---
+++ Select-ScriptCommands.ps1
@@ -0,0 +1,58 @@
+<#
+.SYNOPSIS
+Returns the commands used by the specified script.
+
+.FUNCTIONALITY
+Scripts
+
+.INPUTS
+System.String containing the path to a script file to parse.
+
+.OUTPUTS
+System.Management.Automation.CommandInfo for each command parsed from the file.
+
+.LINK
+https://learn.microsoft.com/dotnet/api/system.management.automation.language.parser.parsefile
+
+.LINK
+Get-Command
+
+.EXAMPLE
+Select-ScriptCommands.ps1 Select-ScriptCommands.ps1
+
+CommandType  Name                Version    Source
+-----------  ----                -------    ------
+Cmdlet       Out-Null            7.5.0.500  Microsoft.PowerShell.Core
+Cmdlet       Where-Object        7.5.0.500  Microsoft.PowerShell.Core
+Cmdlet       Select-Object       7.0.0.0    Microsoft.PowerShell.Utility
+Cmdlet       Get-Command         7.5.0.500  Microsoft.PowerShell.Core
+Cmdlet       Resolve-Path        7.0.0.0    Microsoft.PowerShell.Management
+Filter       Get-ScriptCommands
+#>
+
+#Requires -Version 7
+[CmdletBinding()][OutputType([System.Management.Automation.CommandInfo])] Param(
+# A script file path (wildcards are accepted).
+[Parameter(Position=0,ValueFromPipeline=$true)][string] $Path
+)
+Begin
+{
+    $Script:parseErrors = [Management.Automation.Language.ParseError[]]@()
+    $Script:tokens = [Management.Automation.Language.Token[]]@()
+    filter Get-ScriptCommands
+    {
+        [CmdletBinding()] Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)][string] $Path
+        )
+        [Management.Automation.Language.Parser]::ParseFile($Path,
+            [ref]$Script:tokens, [ref]$Script:parseErrors) |Out-Null
+        $Script:tokens |
+            Where-Object TokenFlags -eq 'CommandName' |
+            Select-Object -Unique -ExpandProperty Value |
+            Get-Command
+    }
+}
+Process
+{
+    Resolve-Path $Path |Get-ScriptCommands
+}
