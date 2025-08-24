adb4dd43ac672a09c8302b14ab794493d79ca4cf
Brian Lalonde <brianary@gmail.com>
2025-08-23 21:11:24 -0700

    ✨ Add `Show-Status.ps1`
    - resolves #83

--- Test-Administrator.ps1
+++ Test-Administrator.ps1
@@ -4,8 +4,17 @@ Checks whether the current session has administrator privileges.
 
 .FUNCTIONALITY
 PowerShell
+
+.EXAMPLE
+Test-Administrator.ps1
+
+False
 #>
 
 #Requires -Version 2
-([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).`
-	IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if($IsWindows)
+{
+	return ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).`
+		IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+else {return [Environment]::IsPrivilegedProcess}
