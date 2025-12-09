7f8a2f496432ef87abda9cf4f8cd0e41fd883bae
Brian Lalonde <brianary@gmail.com>
2025-12-08 20:46:01 -0800

    ⚡️ Improve `Test-WindowsTerminal.ps1`
    - resolves #99

--- Test-WindowsTerminal.ps1
+++ Test-WindowsTerminal.ps1
@@ -1,13 +1,25 @@
 <#
 .SYNOPSIS
 Returns true if PowerShell is running within Windows Terminal.
+
+.FUNCTIONALITY
+Windows Terminal
+
+.LINK
+https://aka.ms/terminal-documentation
+
+.EXAMPLE
+Test-WindowsTerminal.ps1
+
+True
 #>
 
 #Requires -Version 7
-[CmdletBinding()] Param()
+[CmdletBinding()][OutputType([bool])] Param()
 if(!$IsWindows) {return $false}
+if($env:WT_SESSION) {return $true}
 for($process = Get-Process -Id $PID; $process; $process = $process.Parent)
 {
-    if($process.ProcessName -eq 'WindowsTerminal') {return $true}
+	if($process.ProcessName -eq 'WindowsTerminal') {return $true}
 }
 return $false
