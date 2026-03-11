2d3433209d8648d008872c9c0b5ebd8626874b3b
Brian Lalonde <brianary@gmail.com>
2026-03-10 21:27:42 -0700

    ✅ Finally fix package export test

--- test/Export-InstalledPackages.Tests.ps1
+++ test/Export-InstalledPackages.Tests.ps1
@@ -12,25 +12,29 @@ Describe 'Export-InstalledPackages' -Tag Export-InstalledPackages -Skip:$skip {
 		$scriptsdir,$sep = (Split-Path $PSScriptRoot),[io.path]::PathSeparator
 		if($scriptsdir -notin ($env:Path -split $sep)) {$env:Path += "$sep$scriptsdir"}
 		$installed = @{
-			psmodules       = 'Get-Module'
-			winget          = 'winget'
-			choco           = 'choco'
-			'scoop-buckets' = 'scoop-buckets'
-			npm             = 'npm'
-			'dotnet-tools'  = 'dotnet'
-			'gh-extensions' = 'gh'
+			psmodules           = 'Get-Module'
+			winget              = 'winget'
+			choco               = 'choco'
+			'scoop-buckets'     = 'scoop'
+			npm                 = 'npm'
+			'dotnet-tools'      = 'dotnet'
+			'gh-extensions'     = 'gh'
+			'vscode-extensions' = 'code'
 		}
 		@($installed.Keys) |Where-Object {!(Get-Command $_ -ErrorAction Ignore)} |ForEach-Object {$installed.Remove($_)}
 		function winget {'{Sources:{Packages:{PackageIdentifier:["winget"]}}}' |Out-File "$([io.path]::GetTempPath())\winget.json"}
-		function scoop {[pscustomobject]@{Name='scoop'}}
+		function scoop { Param([Parameter(Position=0,Mandatory=$true)][string]$Command,[Parameter(Position=1)][string]$Param)
+			switch($Command) { list {return [pscustomobject]@{Name='scoop'}} bucket {return [pscustomobject]@{Name='scoop-buckets'}} }}
 		function npm {'{dependencies:{npm:true}}'}
 		Mock Get-Module {[pscustomobject]@{Name='psmodules'}}
 		Mock winget {'{Sources:{Packages:{PackageIdentifier:["winget"]}}}' |Out-File "$([io.path]::GetTempPath())\winget.json"} -ErrorAction Ignore
 		Mock choco {'','choco',''} -ErrorAction Ignore
+		Mock scoop {[pscustomobject]@{Name='scoop-buckets'}} -ParameterFilter {$Command -eq 'bucket'} -ErrorAction Ignore
 		Mock scoop {[pscustomobject]@{Name='scoop'}} -ErrorAction Ignore
 		Mock npm {'{dependencies:{npm:true}}'} -ErrorAction Ignore
 		Mock dotnet {'','','dotnet-tools version'} -ErrorAction Ignore
 		Mock gh {'- - gh-extensions -'} -ErrorAction Ignore
+		Mock code {'vscode-extensions'}
 	}
 	Context 'Exports the list of packages installed by various tools' `
 		-Tag ExportInstalledPackages,Export,InstalledPackages,Packages {
