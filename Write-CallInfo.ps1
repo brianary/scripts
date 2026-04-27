f5c3cfb9b0e990125fa8e159edcad2fbe4ba7469
Brian Lalonde <brianary@gmail.com>
2026-04-26 21:40:33 -0700

    🔥 Remove ModernConveniences commands

--- Write-CallInfo.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-Prints caller name and parameters to the host for debugging purposes.
-#>
-
-#Requires -Version 3
-[CmdletBinding()] Param()
-(Get-Variable MyInvocation -ValueOnly -Scope 1).{MyCommand}?.Name ?? '<anonymous>' |Write-Host -ForegroundColor Cyan
-Get-Variable PSBoundParameters -ValueOnly -Scope 1 |ConvertTo-Json -Depth 100 |Write-Host -ForegroundColor DarkGray
